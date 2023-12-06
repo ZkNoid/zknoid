1ead9755739f6c7460daf46a744174e08fa9e69e
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2023-12-06 17:34:46 +0300

    Draw contract ball position

--- apps/web/components/GameView.tsx
+++ apps/web/components/GameView.tsx
@@ -1,294 +1,358 @@
 "use client";
 import { useWalletStore } from "@/lib/stores/wallet";
 import {
+  Brick,
   Bricks,
   GameInputs,
   Tick,
   loadGameContext,
-  defaultLevel
+  defaultLevel,
+  MAX_BRICKS,
 } from "zknoid-chain";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { Int64, PublicKey, UInt64, Bool } from "o1js";
+import { Int64, PublicKey, UInt64, Bool, AccountUpdate } from "o1js";
 import { DUMMY_PROOF } from "@/constants";
 import { Ball, Cart, IBrick } from "@/lib/types";
+import { GameContext } from "zknoid-chain/dist/GameHub";
 
 interface IGameViewProps {
-    gameId: number;
-    onWin: (ticks: number[]) => void;
-    onLost: (ticks: number[]) => void;
-    level: Bricks;
+  gameId: number;
+  onWin: (ticks: number[]) => void;
+  onLost: (ticks: number[]) => void;
+  level: Bricks;
 }
 
 export const GameView = (props: IGameViewProps) => {
-    const canvas = useRef<HTMLCanvasElement>(null);
-    const [ctx, setContext] = useState<
-      CanvasRenderingContext2D | null | undefined
-    >(null);
-    const [animationId, setAnimationId] = useState(-1);
-    const [win, setWin] = useState(false);
-    const [lost, setLost] = useState(false);
-  
-    let ticksCache: number[] = [];
-    let bricksLeft: number = 0;
-  
-    // const [ticks, setTicks] = useState<number[]>([]);
-  
+  const canvas = useRef<HTMLCanvasElement>(null);
+  const [ctx, setContext] = useState<
+    CanvasRenderingContext2D | null | undefined
+  >(null);
+  const [animationId, setAnimationId] = useState(-1);
+  const [win, setWin] = useState(false);
+  const [lost, setLost] = useState(false);
 
-    let lastUpdateTime = Date.now();
-    const tickPeriod = 1000;
-  
-    let ball: Ball;
-    let cart: Cart;
-    let bricks: IBrick[] = [];
-    let stopped: boolean = false;
-  
-    useEffect(() => {
-        if (props.gameId > 0)
-            startGame();
-      }, [props.gameId]);
-  
-    useEffect(() => {
-      const ctx = canvas!.current?.getContext("2d");
-      setContext(ctx);
-    }, [canvas]);
-  
-    const gameLoop = (time: number) => {
-      if (stopped) return;
-  
-      ctx!.clearRect(0, 0, canvas.current!.width, canvas.current!.height);
-      moveCart();
-      moveBall();
-  
-      drawBall();
-      drawBricks();
-  
-      drawCart();
-  
-      if (Date.now() - lastUpdateTime > tickPeriod) {
-        ticksCache.push(1);
-        // setTicks([...ticksCache, 1]);
-        lastUpdateTime = Date.now();
-      }
-  
-      setAnimationId(requestAnimationFrame(gameLoop));
-    };
-  
-    const moveCart = () => {
-      cart.x += cart.dx;
-  
-      if (cart.x > canvas!.current!.width - cart.w) {
-        cart.x = canvas!.current!.width - cart.w;
-      }
-  
-      if (cart.x < 0) {
-        cart.x = 0;
-      }
-    };
-  
-    const moveBall = () => {
-      ball.x += ball.dx;
-      ball.y += ball.dy;
-  
-      if (
-        ball.x + ball.radius > canvas!.current!.width ||
-        ball.x - ball.radius < 0
-      ) {
-        ball.dx *= -1;
-      }
-  
-      if (
-        ball.y + ball.radius > canvas!.current!.height ||
-        ball.y - ball.radius < 0
-      ) {
-        ball.dy *= -1;
-      }
-  
-      if (ball.y + ball.radius > canvas!.current!.height) {
-        return onLost();
-      }
-  
-      if (
-        ball.x - ball.radius > cart.x &&
-        ball.x + ball.radius < cart.x + cart.w &&
-        ball.y + ball.radius > cart.y
-      ) {
-        ball.dy *= -1;
-      }
-  
-      bricks.forEach((brick) => {
-          if (brick.value > 0) {
-            if (
-              ball.x - ball.radius > brick.x &&
-              ball.x + ball.radius < brick.x + brick.w &&
-              ball.y + ball.radius > brick.y &&
-              ball.y - ball.radius < brick.y + brick.h
-            ) {
-              ball.dy *= -1;
-              brick.value = 0;
-              bricksLeft -= 1;
-  
-              if (bricksLeft == 0) {
-                stopped = true;
-                return onWin();
-            }
-          }
-        }
-      });
-    };
-  
-    const drawBall = () => {
-      ctx!.beginPath();
-      ctx!.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
-      ctx!.fillStyle = "black";
-      ctx!.fill();
-      ctx!.closePath();
-    };
-  
-    const drawBricks = () => {
-      bricks.forEach((brick) => {
-          ctx!.beginPath();
-          ctx!.rect(brick.x, brick.y, brick.w, brick.h);
-          ctx!.fillStyle = brick.value > 0 ? "#0095dd" : "transparent";
-          ctx!.fill();
-          ctx!.closePath();
-        });
-    };
-  
-    const drawCart = () => {
-      ctx!.beginPath();
-      ctx!.rect(cart.x, cart.y, cart.w, cart.h);
-      ctx!.fillStyle = "red";
-      ctx!.fill();
-      ctx!.closePath();
-    };
-  
-    const keyDown = (e: KeyboardEvent) => {
-      if (e.key === "Right" || e.key === "ArrowRight") {
-        if (
-          Date.now() - lastUpdateTime > tickPeriod ||
-          ticksCache[ticksCache.length - 1] != 2
-        ) {
-          ticksCache.push(2);
-        //   setTicks([...ticksCache, 2]);
-          lastUpdateTime = Date.now();
-        }
-  
-        cart.dx = 4;
-      } else if (e.key === "Left" || e.key === "ArrowLeft") {
-        if (
-          Date.now() - lastUpdateTime > tickPeriod ||
-          ticksCache[ticksCache.length - 1] != 0
-        ) {
-          ticksCache.push(0);
-        //   setTicks([...ticksCache, 0]);
-          lastUpdateTime = Date.now();
-        }
-  
-        cart.dx = -4;
-      }
-    };
-  
-    const keyUp = (e: KeyboardEvent) => {
-      if (
-        e.key === "Right" ||
-        e.key === "ArrowRight" ||
-        e.key === "Left" ||
-        e.key === "ArrowLeft"
-      ) {
-        cart.dx = 0;
-      }
-    };
+  let ticksCache: number[] = [];
+  let bricksLeft: number = 0;
 
-  
-    const startGame = () => {
-      setLost(false);
-      setWin(false);
-      stopped = false;
-      bricksLeft = props.level.bricks.length;
-  
-      ball = {
-        x: canvas!.current!.width / 2,
-        y: canvas!.current!.height / 2,
-        dx: 4,
-        dy: -3,
-        radius: 3,
-      };
-  
-      cart = {
-        x: canvas!.current!.width / 2,
-        y: canvas!.current!.height - 10,
-        w: 100,
-        h: 20,
-        dx: 0,
-      };
-  
-      const commonBrick = {
-        w: 30,
-        h: 30,
-        gap: 20,
-      };
+  // const [ticks, setTicks] = useState<number[]>([]);
 
-      for (let i = 0; i < bricks.length; i++) {
-      }
+  let lastUpdateTime = Date.now();
+  const tickPeriod = 1000;
 
-      console.log('Level bricks', props.level.bricks);
-  
-      for (let i = 0; i < props.level.bricks.length; i++) {
-        const brickValue = props.level.bricks[i].value * 1;
-        if (brickValue > 0)
-          bricks[i] = { 
-            x: props.level.bricks[i].pos.x * 1, 
-            y: props.level.bricks[i].pos.y * 1, 
-            value: brickValue, 
-            ...commonBrick 
-        };
-      }
+  let gameContext: GameContext; // For updating contractBall position
 
-      console.log(' bricks', bricks);
+  let ball: Ball;
+  let contractBall: Ball;
+  let cart: Cart;
+  let bricks: IBrick[] = [];
+  let stopped: boolean = false;
 
-      if (
-        ball.x - ball.radius > cart.x &&
-        ball.x + ball.radius < cart.x + cart.w &&
-        ball.y + ball.radius > cart.y
-      ) {
-        ball.dy = -ball.dy;
-      }
-  
-      gameLoop(-1);
-  
-      document.addEventListener("keydown", keyDown);
-      document.addEventListener("keyup", keyUp);
-    };
-    const onWin = async () => {
-        stopped = true;
+  useEffect(() => {
+    if (props.gameId > 0) startGame();
+  }, [props.gameId]);
 
-        props.onWin(ticksCache);
-        return;
+  useEffect(() => {
+    const ctx = canvas!.current?.getContext("2d");
+    setContext(ctx);
+  }, [canvas]);
+
+  const gameLoop = (time: number) => {
+    if (stopped) return;
+
+    ctx!.clearRect(0, 0, canvas.current!.width, canvas.current!.height);
+    moveCart();
+    moveBall();
+
+    drawBall();
+    drawContractBall();
+    drawBricks();
+
+    drawCart();
+
+    if (Date.now() - lastUpdateTime > tickPeriod) {
+      pushTick(1);
+      // ticksCache.push(1);
+      // setTicks([...ticksCache, 1]);
+      lastUpdateTime = Date.now();
     }
 
-    const onLost = async () => {
-        stopped = true;
+    setAnimationId(requestAnimationFrame(gameLoop));
+  };
 
-        props.onLost(ticksCache);
-        return;
-        
-        // client.start();
-        // const gameHub = client.runtime.resolve('GameHub');
-        // const sender = PublicKey.fromBase58(address);
-    
-        // const tx1 = await client.transaction(sender, () => {
-        //     gameHub.addGameResult(sender, GameRecordProof.fromJSON(DUMMY_PROOF));
-        // });
-    
-        // await tx1.sign();
-        // await tx1.send();
-    
-        // const sender = PublicKey.fromBase58(address);
-    
-        // const tx = await client.transaction(sender, () => {
-        //   balances.addBalance(sender, UInt64.from(1000));
-        // });
-      };
-    
+  const moveCart = () => {
+    cart.x += cart.dx;
+
+    if (cart.x > canvas!.current!.width - cart.w) {
+      cart.x = canvas!.current!.width - cart.w;
+    }
+
+    if (cart.x < 0) {
+      cart.x = 0;
+    }
+  };
+
+  const moveBall = () => {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    if (
+      ball.x + ball.radius > canvas!.current!.width ||
+      ball.x - ball.radius < 0
+    ) {
+      ball.dx *= -1;
+    }
+
+    if (
+      ball.y + ball.radius > canvas!.current!.height ||
+      ball.y - ball.radius < 0
+    ) {
+      ball.dy *= -1;
+    }
+
+    if (ball.y + ball.radius > canvas!.current!.height) {
+      return onLost();
+    }
+
+    if (
+      ball.x - ball.radius > cart.x &&
+      ball.x + ball.radius < cart.x + cart.w &&
+      ball.y + ball.radius > cart.y
+    ) {
+      ball.dy *= -1;
+    }
+
+    bricks.forEach((brick) => {
+      if (brick.value > 0) {
+        if (
+          ball.x - ball.radius > brick.x &&
+          ball.x + ball.radius < brick.x + brick.w &&
+          ball.y + ball.radius > brick.y &&
+          ball.y - ball.radius < brick.y + brick.h
+        ) {
+          ball.dy *= -1;
+          brick.value = 0;
+          bricksLeft -= 1;
+
+          if (bricksLeft == 0) {
+            stopped = true;
+            return onWin();
+          }
+        }
+      }
+    });
+  };
+
+  const drawBall = () => {
+    ctx!.beginPath();
+    ctx!.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+    ctx!.fillStyle = "black";
+    ctx!.fill();
+    ctx!.closePath();
+  };
+
+  const drawContractBall = () => {
+    ctx!.beginPath();
+    ctx!.arc(
+      contractBall.x,
+      contractBall.y,
+      contractBall.radius,
+      0,
+      Math.PI * 2,
+    );
+    ctx!.strokeStyle = "red";
+    ctx!.stroke();
+    ctx!.closePath();
+  };
+
+  const drawBricks = () => {
+    bricks.forEach((brick) => {
+      ctx!.beginPath();
+      ctx!.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx!.fillStyle = brick.value > 0 ? "#0095dd" : "transparent";
+      ctx!.fill();
+      ctx!.closePath();
+    });
+  };
+
+  const drawCart = () => {
+    ctx!.beginPath();
+    ctx!.rect(cart.x, cart.y, cart.w, cart.h);
+    ctx!.fillStyle = "red";
+    ctx!.fill();
+    ctx!.closePath();
+  };
+
+  const keyDown = (e: KeyboardEvent) => {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+      if (
+        Date.now() - lastUpdateTime >
+        tickPeriod
+        // ticksCache[ticksCache.length - 1] != 2
+      ) {
+        ticksCache.push(2);
+        //   setTicks([...ticksCache, 2]);
+        lastUpdateTime = Date.now();
+      }
+
+      cart.dx = 4;
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+      if (
+        Date.now() - lastUpdateTime >
+        tickPeriod
+        // ticksCache[ticksCache.length - 1] != 0
+      ) {
+        ticksCache.push(0);
+        //   setTicks([...ticksCache, 0]);
+        lastUpdateTime = Date.now();
+      }
+
+      cart.dx = -4;
+    }
+  };
+
+  const keyUp = (e: KeyboardEvent) => {
+    if (
+      e.key === "Right" ||
+      e.key === "ArrowRight" ||
+      e.key === "Left" ||
+      e.key === "ArrowLeft"
+    ) {
+      cart.dx = 0;
+    }
+  };
+
+  const startGame = () => {
+    setLost(false);
+    setWin(false);
+    stopped = false;
+    bricksLeft = props.level.bricks.length;
+
+    ball = {
+      x: canvas!.current!.width / 2,
+      y: canvas!.current!.height / 2,
+      dx: 4,
+      dy: -3,
+      radius: 3,
+    };
+
+    cart = {
+      x: canvas!.current!.width / 2,
+      y: canvas!.current!.height - 10,
+      w: 100,
+      h: 20,
+      dx: 0,
+    };
+
+    const commonBrick = {
+      w: 30,
+      h: 30,
+      gap: 20,
+    };
+
+    for (let i = 0; i < bricks.length; i++) {}
+
+    console.log("Level bricks", props.level.bricks);
+
+    for (let i = 0; i < props.level.bricks.length; i++) {
+      const brickValue = props.level.bricks[i].value * 1;
+      if (brickValue > 0)
+        bricks[i] = {
+          x: props.level.bricks[i].pos.x * 1,
+          y: props.level.bricks[i].pos.y * 1,
+          value: brickValue,
+          ...commonBrick,
+        };
+    }
+
+    console.log(" bricks", bricks);
+
+    /// Contract context init
+    const contractBricks: Bricks = new Bricks({
+      bricks: [...new Array(MAX_BRICKS)].map(
+        (elem) =>
+          new Brick({
+            pos: {
+              x: Int64.from(0),
+              y: Int64.from(0),
+            },
+            value: UInt64.from(1),
+          }),
+      ),
+    });
+
+    for (let i = 0; i < Math.min(props.level.bricks.length, MAX_BRICKS); i++) {
+      contractBricks.bricks[i] = new Brick({
+        pos: {
+          x: Int64.from(bricks[i].x),
+          y: Int64.from(bricks[i].y),
+        },
+        value: UInt64.from(2),
+      });
+    }
+
+    gameContext = loadGameContext(contractBricks, new Bool(true));
+    contractBall = {
+      x: +gameContext.ball.position.x.toString(),
+      y: +gameContext.ball.position.y.toString(),
+      dx: 0,
+      dy: 0,
+      radius: 3,
+    };
+
+    console.log(gameContext.bricks);
+
+    if (
+      ball.x - ball.radius > cart.x &&
+      ball.x + ball.radius < cart.x + cart.w &&
+      ball.y + ball.radius > cart.y
+    ) {
+      ball.dy = -ball.dy;
+    }
+
+    gameLoop(-1);
+
+    document.addEventListener("keydown", keyDown);
+    document.addEventListener("keyup", keyUp);
+  };
+  const onWin = async () => {
+    stopped = true;
+
+    props.onWin(ticksCache);
+    return;
+  };
+
+  const onLost = async () => {
+    stopped = true;
+
+    props.onLost(ticksCache);
+    return;
+
+    // client.start();
+    // const gameHub = client.runtime.resolve('GameHub');
+    // const sender = PublicKey.fromBase58(address);
+
+    // const tx1 = await client.transaction(sender, () => {
+    //     gameHub.addGameResult(sender, GameRecordProof.fromJSON(DUMMY_PROOF));
+    // });
+
+    // await tx1.sign();
+    // await tx1.send();
+
+    // const sender = PublicKey.fromBase58(address);
+
+    // const tx = await client.transaction(sender, () => {
+    //   balances.addBalance(sender, UInt64.from(1000));
+    // });
+  };
+
+  const pushTick = (action: number) => {
+    console.log(`Push ${action}`);
+    ticksCache.push(action);
+    UInt64.from(action);
+    gameContext.processTick(new Tick({ action: UInt64.from(action) }));
+    contractBall.x = gameContext.ball.position.x;
+    contractBall.y = gameContext.ball.position.y;
+  };
+
   return <canvas id="canvas" width="500" height="500" ref={canvas}></canvas>;
-}
+};
