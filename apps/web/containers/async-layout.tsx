3654a4e183f4c7203e5acbc36542af8bf0e28aee
asimaranov <a.simaranov@gmail.com>
2023-12-08 02:04:36 +0530

    Added balances

--- apps/web/containers/async-layout.tsx
+++ apps/web/containers/async-layout.tsx
@@ -1,16 +1,25 @@
 import Header from "@/components/Header";
 import { useClientStore } from "@/lib/stores/client";
 import { useNotifyTransactions, useWalletStore } from "@/lib/stores/wallet";
-import { ReactNode, useEffect, useMemo } from "react";
+import { PublicKey } from "o1js";
+import { ReactNode, useEffect, useMemo, useState } from "react";
 
 export default function AsyncLayout({ children }: { children: ReactNode }) {
   const wallet = useWalletStore();
+  const [balance, setBalance] = useState("0");
   const client = useClientStore();
   useNotifyTransactions();
 
   useEffect(() => {
     client.start();
   }, []);
+
+  useEffect(() => {
+    if (client.client && wallet?.wallet)
+      client.client!.query.runtime.Balances.balances.get(PublicKey.fromBase58(wallet!.wallet!))
+    .then(balance => setBalance(balance?.toString() || "0"));
+  }, [client]);
+
 
   useEffect(() => {
     wallet.initializeWallet();
--- packages/chain/src/client.config.ts
+++ packages/chain/src/client.config.ts
@@ -2,3 +2,4 @@ import { ClientAppChain } from "@proto-kit/sdk";
 import runtime from "./runtime";
 
 export const client = ClientAppChain.fromRuntime(runtime);
+
