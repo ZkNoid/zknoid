1174b6d13b847dde8e86586cde1098ada54e2b1a
asimaranov <a.simaranov@gmail.com>
2023-12-30 01:32:56 +0700

    Added missed footer file

---
+++ apps/web/components/Footer.tsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+    return (
+        <div className="w-full flex flex-row justify-center items-center gap-10 h-20">
+            <a href="https://zknoid.medium.com/" className="flex items-center">Medium</a>
+            <a href="https://github.com/ZkNoid" className="flex items-center">Github</a>
+        </div>
+    )
+}
