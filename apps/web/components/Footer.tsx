2495ef56ac46af72c14d612bf7358899974f2eb6
asimaranov <a.simaranov@gmail.com>
2023-12-31 02:34:34 +0700

    Added docs link to footer

--- apps/web/components/Footer.tsx
+++ apps/web/components/Footer.tsx
@@ -1,8 +1,9 @@
 export const Footer = () => {
     return (
         <div className="w-full flex flex-row justify-center items-center gap-10 h-20">
-            <a href="https://zknoid.medium.com/" className="flex items-center">Medium</a>
-            <a href="https://github.com/ZkNoid" className="flex items-center">Github</a>
+            <a href="https://zknoid.medium.com/">Medium</a>
+            <a href="https://github.com/ZkNoid">Github</a>
+            <a href="https://docs.zknoid.io/">Docs</a>
         </div>
     )
 }
