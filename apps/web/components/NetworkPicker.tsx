d7a013d7e38fee514d374b7443d130a218aedb50
asimaranov <a.simaranov@gmail.com>
2024-01-05 02:05:20 +0700

    Improved network management

--- apps/web/components/NetworkPicker.tsx
+++ apps/web/components/NetworkPicker.tsx
@@ -1,10 +1,76 @@
-export const NetworkPicker = () => {
-    return (
-        <div className="flex flex-col items-center w-40 py-5 absolute bg-slate-300 text-xs gap-7 rounded-xl right-5 top-20">
-                            {["Mainnet", "Devnet", "Berkley", "Testworld2"].map(network => (
-                                <div className="cursor-pointer">{network}</div>
-                                ))}
+import { NETWORKS } from "@/app/constants/networks";
+import { useNetworkStore } from "@/lib/stores/network";
+import { useEffect, useState } from "react";
 
-                            </div>
+export const NetworkPicker = ({autoconnect}: {autoconnect: boolean}) => {
+    const [expanded, setExpanded] = useState(false);
+    const networkStore = useNetworkStore();
+    
+    const switchNetwork = async (chainId: string) => {
+        await (window as any).mina.switchChain({
+            chainId
+        });
+
+        networkStore.setNetwork(chainId);
+    }
+
+    useEffect(() => {
+        (async () => {
+            const network = await (window as any).mina.requestNetwork();
+            networkStore.setNetwork(network.chainId);
+
+            const listener = ({chainId, name}: {chainId: string, name: string}) => {
+                networkStore.setNetwork(chainId);
+            };
+
+            (window.mina as any).on('chainChanged', listener);
+
+            return () => {
+                (window.mina as any).removeListener(listener);
+            }
+        })();
+    }, []);
+
+    useEffect(() => {
+        (async () => {
+            const [account] = await (window as any).mina.requestAccounts();
+
+            networkStore.setAddress(account);
+
+            const listener = (accounts: string[]) => {
+                const [account] = accounts;
+                networkStore.setAddress(account);
+            };
+
+            (window.mina as any).on('accountsChanged', listener);
+
+            return () => {
+                (window.mina as any).removeListener(listener);
+            }
+        })();
+    }, []);
+
+    useEffect(() => {
+        if (autoconnect) {
+            networkStore.connectWallet();
+        }
+    }, []);
+
+    return (
+        <div>
+            <div className="cursor-pointer" onClick={() => setExpanded(!expanded)}>{networkStore.minaNetwork?.name}</div>
+            {expanded && (
+                <div className="flex flex-col items-center w-40 py-5 absolute bg-slate-300  text-xs rounded-xl right-5 top-20">
+                    {NETWORKS.map(network => (
+                        <div
+                            className="cursor-pointer h-full w-full hover:bg-slate-400 py-3 px-7"
+                            onClick={() => switchNetwork(network.chainId)}
+                        >
+                            {network.name}
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
     )
 }
--- apps/web/containers/async-layout.tsx
+++ apps/web/containers/async-layout.tsx
@@ -2,15 +2,15 @@ import { GameType } from "@/app/constants/games";
 import { Footer } from "@/components/Footer";
 import Header from "@/components/Header";
 import { useClientStore } from "@/lib/stores/client";
+import { useNetworkStore } from "@/lib/stores/network";
 import { useNotifyTransactions, useWalletStore } from "@/lib/stores/wallet";
-import { walletInstalled } from "@/lib/utils";
 import { PublicKey } from "o1js";
 import { ReactNode, useEffect, useMemo, useState } from "react";
 
 export default function AsyncLayout({ children }: { children: ReactNode }) {
-  const wallet = useWalletStore();
   const [balance, setBalance] = useState("0");
   const client = useClientStore();
+  const networkStore = useNetworkStore();
   useNotifyTransactions();
 
   useEffect(() => {
@@ -18,27 +18,19 @@ export default function AsyncLayout({ children }: { children: ReactNode }) {
   }, []);
 
   useEffect(() => {
-    if (client.client && wallet?.wallet)
-      client.client!.query.runtime.Balances.balances.get(PublicKey.fromBase58(wallet!.wallet!))
+    if (client.client && networkStore.adderss)
+      client.client!.query.runtime.Balances.balances.get(PublicKey.fromBase58(networkStore.adderss))
     .then(balance => setBalance(balance?.toString() || "0"));
   }, [client]);
 
 
-  useEffect(() => {
-    if (!walletInstalled()) {
-      return;
-    }
-    wallet.initializeWallet();
-    wallet.observeWalletChange();
-  }, []);
-
   return (
     <div className={"flex flex-col min-h-screen"}>
       <Header
-        address={wallet.wallet}
-        onConnectWallet={wallet.connectWallet}
+        address={networkStore.adderss}
+        connectWallet={networkStore.connectWallet}
         balance={parseInt(balance)}
-        walletInstalled={walletInstalled()}
+        walletInstalled={networkStore.walletInstalled()}
         currentGame={GameType.Arkanoid}
       />
       {children}
