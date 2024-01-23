5121dff4b5301c63e9955a26b7708816f7deeacb
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-01-23 17:41:53 +0300

    Added simple competitions list

--- apps/web/components/arkanoid/ArkanoidCompetitionsListPage.tsx
+++ apps/web/components/arkanoid/ArkanoidCompetitionsListPage.tsx
@@ -1,7 +1,25 @@
 'use client';
 
+import Link from 'next/link';
 import { useEffect, useRef, useState } from 'react';
 
+const testList = ['global', 'global', 'global', '1', '2'];
+
 export default function ArkanoidCompetitionsListPage() {
-  return <h1>Really cool page in the future</h1>;
+  let [competitions, setCompetitions] = useState(testList);
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-5 py-10">
+      {competitions.map((c) => (
+        <div className="w-1/5 rounded border-2 border-solid border-blue-500">
+          <Link
+            className="m-5 flex h-full flex-col gap-5"
+            href={`/games/arkanoid/${c}`}
+          >
+            {c}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 }
