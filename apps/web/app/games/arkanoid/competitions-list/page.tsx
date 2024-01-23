58c2875d37ce29bfd68966b455f8b725df8f3389
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-01-23 17:19:13 +0300

    Added dummie competitions list page for arkanoid

---
+++ apps/web/app/games/arkanoid/competitions-list/page.tsx
@@ -0,0 +1,5 @@
+import ArkanoidCompetitionsListPage from '@/components/arkanoid/ArkanoidCompetitionsListPage';
+
+export default function ArkanoidCompetitionsListPage1() {
+  return <ArkanoidCompetitionsListPage />;
+}
---
+++ apps/web/components/arkanoid/ArkanoidCompetitionsListPage.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
+
+export default function ArkanoidCompetitionsListPage() {
+  return <h1>Really cool page in the future</h1>;
+}
