3147d5b8c8a8cc8f470173272942373c1ce51e55
asimaranov <a.simaranov@gmail.com>
2024-01-24 00:58:02 +0700

    Added missed file

---
+++ apps/web/lib/stores/randzu/sessionKeyStorage.tsx
@@ -0,0 +1,30 @@
+import { create } from "zustand";
+import { PrivateKey, PublicKey, UInt32, UInt64 } from "o1js";
+import { persist } from "zustand/middleware";
+
+export interface MatchQueueState {
+    sessionKeyBase58: any;
+    getSessionKey: () => PrivateKey;
+    newSessionKey: () => PrivateKey;
+}
+
+export const useSessionKeyStore = create<MatchQueueState, [["zustand/persist", never]]>(
+  persist(
+    (set, get) => ({
+        sessionKeyBase58: PrivateKey.random().toBase58(),
+        getSessionKey() {
+            return PrivateKey.fromBase58(this.sessionKeyBase58)
+        },
+        newSessionKey() {
+            const newPK = PrivateKey.random();
+            set({
+                sessionKeyBase58: newPK.toBase58()
+            });
+            return newPK;
+        },
+    }),
+    {
+      name: 'sessionStorage',
+    },
+  ),
+)
