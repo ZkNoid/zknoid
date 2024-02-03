a9049405e1b9dc125cf971fb5594c64ebbef68a8
asimaranov <a.simaranov@gmail.com>
2024-02-03 13:52:10 +0700

    Added matchmaker to the engine. Added docstrings. Added missed files

---
+++ packages/chain/src/engine/GameHub.ts
@@ -0,0 +1,155 @@
+import {
+    RuntimeModule,
+    runtimeModule,
+    state,
+    runtimeMethod,
+} from '@proto-kit/module';
+import { State, StateMap } from '@proto-kit/protocol';
+import {
+    UInt64,
+    SelfProof,
+    PublicKey,
+    Provable,
+} from 'o1js';
+import {
+    Competition,
+    GameRecordKey,
+    LeaderboardIndex,
+    LeaderboardScore,
+} from '../types';
+
+import { Balances } from '../balances';
+
+import { inject } from 'tsyringe';
+
+export interface IScoreable {
+    score: UInt64;
+}
+
+@runtimeModule()
+export class Gamehub<
+    PublicInput, 
+    PublicOutput extends IScoreable, 
+    Proof extends SelfProof<PublicInput, PublicOutput>, 
+> 
+    extends RuntimeModule<unknown> 
+{
+    // CompetitionId -> competition
+    @state() public competitions = StateMap.from<UInt64, Competition>(
+        UInt64,
+        Competition
+    );
+    @state() public lastCompetitonId = State.from<UInt64>(UInt64);
+
+    /// compettitionId + User => Record
+    @state() public gameRecords = StateMap.from<GameRecordKey, UInt64>(
+        GameRecordKey,
+        UInt64
+    );
+    /// (competitionId, Unsorted index) => user result
+    @state() public leaderboard = StateMap.from<
+        LeaderboardIndex,
+        LeaderboardScore
+    >(LeaderboardIndex, LeaderboardScore);
+    @state() public seeds = StateMap.from<UInt64, UInt64>(UInt64, UInt64);
+    @state() public lastSeed = State.from<UInt64>(UInt64);
+    @state() public lastUpdate = State.from<UInt64>(UInt64);
+
+    public leaderboardSize = 10;
+
+    public constructor(@inject('Balances') private balances: Balances) {
+        super();
+    }
+
+    @runtimeMethod()
+    public updateSeed(seed: UInt64): void {
+        const lastSeedIndex = this.lastSeed.get().orElse(UInt64.from(0));
+        this.seeds.set(lastSeedIndex, seed);
+        this.lastSeed.set(lastSeedIndex.add(1));
+    }
+
+    /**
+     * Creates new game competition
+     *
+     * @param competition - Competition to create
+     */
+    @runtimeMethod()
+    public createCompetition(competition: Competition): void {
+        this.competitions.set(
+            this.lastCompetitonId.get().orElse(UInt64.from(0)),
+            competition
+        );
+        this.lastCompetitonId.set(
+            this.lastCompetitonId.get().orElse(UInt64.from(0)).add(1)
+        );
+    }
+
+    /**
+     * Adds game record to a competition
+     *
+     * @param competitionId - Competition id to add record to
+     * @param gameRecordProof - Proof of the game fairness
+     */
+    @runtimeMethod()
+    public addGameResult(
+        competitionId: UInt64,
+        gameRecordProof: Proof
+    ): void {
+        gameRecordProof.verify();
+
+        const gameKey = new GameRecordKey({
+            competitionId,
+            player: this.transaction.sender,
+        });
+
+        const currentScore = this.gameRecords.get(gameKey).value;
+        const newScore = gameRecordProof.publicOutput.score;
+
+        if (currentScore < newScore) {
+            // Do we need provable here?
+            this.gameRecords.set(gameKey, newScore);
+
+            let looserIndex = UInt64.from(0);
+            let looserScore = UInt64.from(0);
+
+            for (let i = 0; i < this.leaderboardSize; i++) {
+                const leaderboardKey = new LeaderboardIndex({
+                    competitionId,
+                    index: UInt64.from(i),
+                });
+                const gameRecord = this.leaderboard.get(leaderboardKey);
+
+                const result = gameRecord.orElse(
+                    new LeaderboardScore({
+                        score: UInt64.from(0),
+                        player: PublicKey.empty(),
+                    })
+                );
+
+                looserIndex = Provable.if(
+                    result.score.lessThan(looserScore),
+                    UInt64.from(i),
+                    looserIndex
+                );
+                looserScore = Provable.if(
+                    result.score.lessThan(looserScore),
+                    UInt64.from(i),
+                    looserScore
+                );
+            }
+
+            const looserKey = new LeaderboardIndex({
+                competitionId,
+                index: looserIndex,
+            });
+
+            this.leaderboard.set(
+                looserKey,
+                new LeaderboardScore({
+                    score: newScore,
+                    player: this.transaction.sender,
+                })
+            );
+        }
+    }
+}
---
+++ packages/chain/src/engine/MatchMaker.ts
@@ -0,0 +1,125 @@
+import {
+  RuntimeModule,
+  runtimeModule,
+  state,
+  runtimeMethod,
+} from "@proto-kit/module";
+import { Option, StateMap, assert } from "@proto-kit/protocol";
+import { PublicKey, Struct, UInt64, Provable, Bool, UInt32, Poseidon, Field, Int64 } from "o1js";
+
+interface MatchMakerConfig { }
+
+const PENDING_BLOCKS_NUM = UInt64.from(5);
+
+export class RoundIdxUser extends Struct({
+  roundId: UInt64,
+  userAddress: PublicKey
+}) { }
+
+export class RoundIdxIndex extends Struct({
+  roundId: UInt64,
+  index: UInt64
+}) { }
+
+export class AddressxAddress extends Struct({
+  user1: PublicKey,
+  user2: PublicKey
+}) { }
+
+export class QueueListItem extends Struct({
+  userAddress: PublicKey,
+  registrationTimestamp: UInt64
+}) { }
+
+@runtimeModule()
+export class MatchMaker extends RuntimeModule<MatchMakerConfig> {
+  // Session => user
+  @state() public sessions = StateMap.from<PublicKey, PublicKey>(
+    PublicKey,
+    PublicKey
+  );
+  // mapping(roundId => mapping(registered user address => bool))
+  @state() public queueRegisteredRoundUsers = StateMap.from<RoundIdxUser, Bool>(
+    RoundIdxUser,
+    Bool
+  );
+  // mapping(roundId => SessionKey[])
+  @state() public queueRoundUsersList = StateMap.from<RoundIdxIndex, QueueListItem>(
+    RoundIdxIndex,
+    QueueListItem
+  );
+  @state() public queueLength = StateMap.from<UInt64, UInt64>(
+    UInt64,
+    UInt64
+  );
+
+  @state() public activeGameId = StateMap.from<PublicKey, UInt64>(
+    PublicKey,
+    UInt64
+  );
+
+  /**
+   * Initializes game when opponent is found
+   *
+   * @param opponentReady - Is opponent found. If not ready, function call should process this case without initialization
+   * @param opponent - Opponent if opponent is ready
+   * @returns Id of the new game. Will be set for player and opponent
+   */
+  public initGame(opponentReady: Bool, opponent: Option<QueueListItem>): UInt64 {
+    return UInt64.from(0);
+  }
+
+  /**
+   * Registers user in session queue
+   *
+   * @param sessionKey - Key of user background session
+   * @param timestamp - Current user timestamp from front-end
+   */
+  @runtimeMethod()
+  public register(sessionKey: PublicKey, timestamp: UInt64): void {
+    // If player in game – revert
+    assert(this.activeGameId.get(this.transaction.sender).orElse(UInt64.from(0)).equals(UInt64.from(0)), "Player already in game");
+
+    // Registering player session key
+    this.sessions.set(sessionKey, this.transaction.sender);
+    const roundId = this.network.block.height.div(PENDING_BLOCKS_NUM);
+
+    // User can't re-register in round queue if already registered
+    assert(
+      this.queueRegisteredRoundUsers.get(new RoundIdxUser({ roundId, userAddress: this.transaction.sender })).isSome.not(),
+      "User already in queue"
+    );
+
+    const queueLength = this.queueLength.get(roundId).orElse(UInt64.from(0));
+
+    const opponentReady = queueLength.greaterThan(UInt64.from(0));
+    const opponent = this.queueRoundUsersList.get(
+      new RoundIdxIndex({ roundId, index: queueLength.sub(Provable.if(opponentReady, UInt64.from(1), UInt64.from(0))) })
+    );
+
+    const gameId = this.initGame(opponentReady, opponent);
+
+    // Assigning new game to player if opponent found
+    this.activeGameId.set(this.transaction.sender, Provable.if(opponentReady, gameId, UInt64.from(0)));
+
+    // Setting that opponent is in game if opponent found
+    this.activeGameId.set(Provable.if(opponentReady, opponent.value.userAddress, PublicKey.empty()), gameId);
+
+    // If opponent not found – adding current user to the list
+    this.queueRoundUsersList.set(
+      new RoundIdxIndex({ roundId, index: queueLength }),
+      new QueueListItem({ userAddress: Provable.if(opponentReady, PublicKey.empty(), this.transaction.sender), registrationTimestamp: timestamp })
+    );
+
+    // If opponent not found – registeting current user in the list
+    this.queueRegisteredRoundUsers.set(
+      new RoundIdxUser(
+        { roundId, userAddress: Provable.if(opponentReady, PublicKey.empty(), this.transaction.sender) }
+      ),
+      Bool(true)
+    );
+
+    // If opponent not found – incrementing queue length. If found – removing opponent by length decreasing 
+    this.queueLength.set(roundId, (Provable.if(opponentReady, queueLength.sub(Provable.if(opponentReady, UInt64.from(1), UInt64.from(0))), queueLength.add(1))));
+  }
+}
