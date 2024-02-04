1475ba1bb1deeb59455f56f8b0159f965b5f9cf9
asimaranov <a.simaranov@gmail.com>
2024-02-04 07:54:39 +0700

    Added docker config for server

--- packages/chain/src/chain.config.ts
+++ packages/chain/src/chain.config.ts
@@ -1,22 +1,131 @@
 import { LocalhostAppChain } from "@proto-kit/cli";
 import runtime from "./runtime";
-import { UInt64 } from "o1js";
+import { Runtime } from "@proto-kit/module";
+import { VanillaProtocol } from "@proto-kit/protocol";
 
-const appChain = LocalhostAppChain.fromRuntime(runtime);
+import {
+  BlockProducerModule,
+  InMemoryDatabase,
+  LocalTaskQueue,
+  LocalTaskWorkerModule,
+  ManualBlockTrigger,
+  NoopBaseLayer,
+  PrivateMempool,
+  Sequencer,
+  SequencerModule,
+  UnprovenProducerModule,
+  sequencerModule,
+} from "@proto-kit/sequencer";
+import {
+  BlockStorageResolver,
+  GraphqlSequencerModule,
+  GraphqlServer,
+  MempoolResolver,
+  NodeStatusResolver,
+  QueryGraphqlModule,
+  UnprovenBlockResolver,
+} from "@proto-kit/api";
+
+import {
+  StateServiceQueryModule,
+  BlockStorageNetworkStateModule,
+} from "@proto-kit/sdk";
+
+@sequencerModule()
+class StartupScripts extends SequencerModule {
+  constructor() {
+    super();
+  }
+  async start(): Promise<void> {
+  }
+}
+
+const appChain = LocalhostAppChain.from({
+  runtime: Runtime.from(runtime),
+
+  protocol: VanillaProtocol.from({}),
+
+  sequencer: Sequencer.from({
+    modules: {
+      Database: InMemoryDatabase,
+      Mempool: PrivateMempool,
+      GraphqlServer,
+      LocalTaskWorkerModule,
+      BaseLayer: NoopBaseLayer,
+      BlockProducerModule,
+      UnprovenProducerModule,
+      BlockTrigger: ManualBlockTrigger,
+      TaskQueue: LocalTaskQueue,
+      Graphql: GraphqlSequencerModule.from({
+        modules: {
+          MempoolResolver,
+          QueryGraphqlModule,
+          BlockStorageResolver,
+          NodeStatusResolver,
+          UnprovenBlockResolver,
+        },
+      }),
+      StartupScripts: StartupScripts,
+    },
+  }),
+
+  modules: {
+    QueryTransportModule: StateServiceQueryModule,
+    NetworkStateTransportModule: BlockStorageNetworkStateModule,
+  },
+});
+
+appChain.configure({
+  ...appChain.config,
+
+  Protocol: {
+    BlockProver: {},
+    StateTransitionProver: {},
+    AccountState: {},
+    BlockHeight: {},
+  },
+
+  Sequencer: {
+    Database: {},
+    UnprovenProducerModule: {},
+    StartupScripts: {},
+
+    GraphqlServer: {
+      port: 8080,
+      host: "0.0.0.0",
+      graphiql: true,
+    },
+
+    Graphql: {
+      QueryGraphqlModule: {},
+      MempoolResolver: {},
+      BlockStorageResolver: {},
+      NodeStatusResolver: {},
+      UnprovenBlockResolver: {},
+    },
+
+    Mempool: {},
+    BlockProducerModule: {},
+    LocalTaskWorkerModule: {},
+    BaseLayer: {},
+    TaskQueue: {},
+    BlockTrigger: {},
+  },
+
+  QueryTransportModule: {},
+  NetworkStateTransportModule: {},
+});
 
 appChain.configure({
   ...appChain.config,
 
   Runtime: {
-    Balances: {
-      totalSupply: UInt64.from(10_000),
-    },
-    ArkanoidGameHub: {
-    },
-    RandzuLogic: {
-    }
-
+    Balances: { },
+    ArkanoidGameHub: { },
+    RandzuLogic: { }
   },
+  
 });
+
 
 export default appChain as any;
--- packages/chain/src/client.config.ts
+++ packages/chain/src/client.config.ts
@@ -2,4 +2,9 @@ import { ClientAppChain } from "@proto-kit/sdk";
 import runtime from "./runtime";
 
 export const client = ClientAppChain.fromRuntime(runtime);
+client.configurePartial({
+    GraphqlClient: {
+        url: process.env.PROTOKIT_URL || "http://localhost:8080/graphql",
+      },
+})
 
