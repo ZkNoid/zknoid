bc69124e65f1ee132ab5b18636b38247314444ef
asimaranov <a.simaranov@gmail.com>
2024-02-05 00:41:38 +0700

    Fixed default competitions creation

--- packages/chain/src/client.config.ts
+++ packages/chain/src/client.config.ts
@@ -4,7 +4,7 @@ import runtime from "./runtime";
 export const client = ClientAppChain.fromRuntime(runtime);
 client.configurePartial({
     GraphqlClient: {
-        url: process.env.NEXT_PUBLIC_PROTOKIT_URL || "http://localhost:8080/graphql",
+        url: process.env.NEXT_PUBLIC_PROTOKIT_URL || "http://127.0.0.1:8080/graphql",
       },
 })
 
