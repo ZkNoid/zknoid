69cd9003a87b0ae557bbd9caab29a77235f6cfdc
asimaranov <a.simaranov@gmail.com>
2024-02-04 14:25:06 +0700

    Fixed protokit url variable

--- packages/chain/src/client.config.ts
+++ packages/chain/src/client.config.ts
@@ -4,7 +4,7 @@ import runtime from "./runtime";
 export const client = ClientAppChain.fromRuntime(runtime);
 client.configurePartial({
     GraphqlClient: {
-        url: process.env.PROTOKIT_URL || "http://localhost:8080/graphql",
+        url: process.env.NEXT_PUBLIC_PROTOKIT_URL || "http://localhost:8080/graphql",
       },
 })
 
