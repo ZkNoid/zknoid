7d09eceba737dbb42b575d2247705066811019ff
asimaranov <a.simaranov@gmail.com>
2024-02-11 23:26:07 +0700

    Removed old import

--- apps/web/containers/async-layout.tsx
+++ apps/web/containers/async-layout.tsx
@@ -1,13 +1,5 @@
-import { GameType } from "@/app/constants/games";
-import Header from "@/components/Header";
-import { useClientStore } from "@/lib/stores/client";
-import { useMinaBalancesStore, useObserveMinaBalance } from "@/lib/stores/minaBalances";
-import { usePollMinaBlockHeight } from "@/lib/stores/minaChain";
-import { useNetworkStore } from "@/lib/stores/network";
-import { useObserveProtokitBalance, useProtokitBalancesStore } from "@/lib/stores/protokitBalances";
-import { usePollProtokitBlockHeight } from "@/lib/stores/protokitChain";
-import { PublicKey } from "o1js";
-import { ReactNode, useEffect, useMemo, useState } from "react";
+
+import { ReactNode } from "react";
 
 export default function AsyncLayout({ children }: { children: ReactNode }) {
 
