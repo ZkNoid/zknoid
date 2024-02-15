7697c1b98e7da69d75643198ba0fe5962963b8ca
asimaranov <a.simaranov@gmail.com>
2024-02-15 19:42:55 +0700

    Fixed title

--- apps/web/games/arkanoid/page.tsx
+++ apps/web/games/arkanoid/page.tsx
@@ -5,6 +5,7 @@ import { arkanoidConfig } from "./config";
 import { getZkNoidGameClient } from "@/lib/createConfig";
 import { AppChainClientContext } from "@/lib/contexts/AppChainClientContext";
 import ArkanoidPage from "./components/ArkanoidPage";
+import Head from "next/head";
 
 export default function Page({
     params,
@@ -14,6 +15,9 @@ export default function Page({
     const client = useMemo(() => getZkNoidGameClient(arkanoidConfig), []);
     return (
         <AppChainClientContext.Provider value={client}>
+            <Head>
+                <title>ZkNoid – Arkanoid game</title>
+            </Head>
             <ArkanoidPage params={{
                 competitionId: params.competitionId
             }} />
