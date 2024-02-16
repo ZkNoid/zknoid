ad5b9340edba49be590f4b5dc3bf8791192c16fb
asimaranov <a.simaranov@gmail.com>
2024-02-16 21:28:09 +0700

    Attempt to fix strange proto-kit bug

--- apps/web/lib/createConfig.ts
+++ apps/web/lib/createConfig.ts
@@ -1,9 +1,23 @@
 import {
+  InMemoryStateService,
+  Runtime,
   RuntimeModulesRecord,
 } from "@proto-kit/module";
-import { ClientAppChain } from '@proto-kit/sdk';
-import { ArkanoidGameHub, client } from 'zknoid-chain-dev';
+import { AuroSigner, ClientAppChain, GraphqlClient, GraphqlQueryTransportModule, GraphqlTransactionSender } from '@proto-kit/sdk';
+import { Sequencer } from "@proto-kit/sequencer";
+import { ArkanoidGameHub } from 'zknoid-chain-dev';
 import { createStore } from "zustand";
+import {
+  AccountStateModule,
+  BlockProver,
+  Protocol,
+  ProtocolModulesRecord,
+  StateServiceProvider,
+  StateTransitionProver,
+  VanillaProtocol,
+} from "@proto-kit/protocol";
+import { GraphqlNetworkStateTransportModule } from "@proto-kit/sdk/dist/graphql/GraphqlNetworkStateTransportModule";
+
 
 export type ZkNoidGameConfig<RuntimeModules extends RuntimeModulesRecord = RuntimeModulesRecord
 > = {
@@ -50,22 +64,75 @@ export function createConfig<
       const modules = Object.assign({}, ...gameModules);
 
       console.log('Loaded modules', modules);
-      
-      const client = ClientAppChain.fromRuntime({
-        modules,
-        config: modules
-      } as {modules: any});
+
+      const runtime = Runtime.from({
+        ...{modules},
+      });
+  
+      const sequencer = Sequencer.from({
+        modules: {},
+      });
+  
+      const appChain = new ClientAppChain({
+        runtime,
+        sequencer,
+        protocol: VanillaProtocol.from({}),
+  
+        modules: {
+          GraphqlClient,
+          Signer: AuroSigner,
+          TransactionSender: GraphqlTransactionSender,
+          QueryTransportModule: GraphqlQueryTransportModule,
+          NetworkStateTransportModule: GraphqlNetworkStateTransportModule,
+        },
+      });
+  
+      appChain.configure({
+        Protocol: {
+          BlockProver: {},
+          StateTransitionProver: {},
+          AccountState: {},
+          BlockHeight: {},
+        },
+  
+        Signer: {},
+        TransactionSender: {},
+        QueryTransportModule: {},
+        NetworkStateTransportModule: {},
+  
+        GraphqlClient: {
+          url: "http://127.0.0.1:8080/graphql",
+        },
+      });
+  
+      /**
+       * Register state service provider globally,
+       * to avoid providing an entire sequencer.
+       *
+       * Alternatively we could register the state service provider
+       * in runtime's container, but i think the event emitter proxy
+       * instantiates runtime/runtime modules before we can register
+       * the mock state service provider.
+       */
+      const stateServiceProvider = new StateServiceProvider();
+      stateServiceProvider.setCurrentStateService(new InMemoryStateService());
+      // container.registerInstance("StateServiceProvider", stateServiceProvider);
+      const client = appChain;
 
       client.configure({
-        Runtime: modules
+        Runtime: {
+          ArkanoidGameHub: {},
+          Balances: {},
+          RandzuLogic: {}
+        }
       });
 
       client.configurePartial({
         GraphqlClient: {
-            url: process.env.NEXT_PUBLIC_PROTOKIT_URL || "http://127.0.0.1:8080/graphql",
-          },
+          url: process.env.NEXT_PUBLIC_PROTOKIT_URL || "http://127.0.0.1:8080/graphql",
+        },
       })
-      
+
       return client;
     },
   }
