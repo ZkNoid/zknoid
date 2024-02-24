0e306de05de6976c47cab3ecbb42d6af6b2958da
asimaranov <a.simaranov@gmail.com>
2024-02-25 02:57:57 +0700

    Added game events, filters

--- apps/web/components/ui/games-store/Section2.tsx
+++ apps/web/components/ui/games-store/Section2.tsx
@@ -2,36 +2,49 @@ import { IGame } from "@/app/constants/games"
 import Link from "next/link"
 import Image from "next/image"
 
-import { ReactNode } from "react"
+import { useState } from "react"
 import { GAME_TAGS } from "@/app/constants/tags"
-import { useFilters } from "@/lib/stores/games-store/filters"
+import { GAME_EVENTS, ZkNoidEvent, ZkNoidEventType, getEventType, useEventTimer } from "@/lib/events"
 
-const FilterCard = ({ text, checked }: { text: string, checked?: boolean }) => (
-    <div className={`p-2 rounded text-[16px] cursor-pointer ${checked ? 'bg-left-accent text-bg-dark' : 'border'}`}>
-        {text}
+const FilterCard = ({ eventType, selected, typesSelected, setTypesSelected }: {
+    eventType: ZkNoidEventType,
+    typesSelected: ZkNoidEventType[],
+    setTypesSelected: (types: ZkNoidEventType[]) => void,
+    selected?: boolean
+}) => (
+    <div
+        onClick={
+            () => setTypesSelected(typesSelected.includes(eventType) ?
+                typesSelected.filter(x => x != eventType) :
+                [...typesSelected, eventType])}
+        className={`p-2 rounded text-[16px] cursor-pointer ${selected ? 'bg-left-accent text-bg-dark border-left-accent' : ''}`}
+    >
+        {eventType}
     </div>
 )
 
-const EventCard = ({ headText, description, startsIn }: { headText: string, description: string, startsIn: string }) => (
-    <div className="border-left-accent flex flex-col relative">
-        <img src="/image/section2/event-box.svg"></img>
-        <div className="absolute -z-10 flex flex-col top-0 left-0 w-full h-full p-5">
-            <div className="text-[24px] font-bold">{headText}</div>
-            <div className="text-[16px] text-plexmono max-w-[462px]">
-                {description}
-            </div>
-            <div className="flex-grow"></div>
-            <div className="text-plexmono max-w-[462px] text-[30px]">
-                START IN: {startsIn}
+const EventCard = ({ headText, description, event }: { headText: string, description: string, event: ZkNoidEvent }) => {
+    const eventCounter = useEventTimer(event);
+    const time = `${eventCounter.startsIn.days}d:${eventCounter.startsIn.hours}h:${eventCounter.startsIn.minutes}m:${Math.trunc(eventCounter.startsIn.seconds!)}s`
+    return (
+        <div className="border-left-accent flex flex-col relative">
+            <img src="/image/section2/event-box.svg" className="-z-10"></img>
+            <div className="absolute flex flex-col top-0 left-0 w-full h-full p-5">
+                <div className="text-[24px] font-bold">{headText}</div>
+                <div className="text-[16px] text-plexmono max-w-[462px]">
+                    {description}
+                </div>
+                <div className="flex-grow"></div>
+                <div className="text-plexmono max-w-[462px] text-[30px]">
+                    {eventCounter.type == ZkNoidEventType.UPCOMING_EVENTS && <>START IN {time}</>}
+                    {eventCounter.type == ZkNoidEventType.CURRENT_EVENTS && <>END IN {time}</>}
+                </div>
             </div>
         </div>
-
-    </div>
-)
+    )
+}
 
 const FiltrationBox = ({ expanded, title, variants }: { expanded: boolean, title: string, variants: string[] }) => {
-    const filters = useFilters();
-
     return (
         <div className="relative p-5 w-full min-h-[80px]">
             <div className="font-bold text-[24px]">
@@ -40,9 +53,7 @@ const FiltrationBox = ({ expanded, title, variants }: { expanded: boolean, title
             {variants.map(variant => (
                 <div
                     className="text-plexmono font-[20] cursor-pointer hover:underline decoration-left-accent underline-offset-[5px]"
-                    onClick={() => {
-                        filters.setGenre(variant);
-                    }}
+                    onClick={() => {}}
                 >
                     {variant}
                 </div>
@@ -70,105 +81,113 @@ const GenreCard = ({ image, text }: { image: string, text: string }) => (
     </div>
 )
 
-export const Section2 = ({ games }: { games: IGame[] }) => (
-    <div className='relative flex flex-col'>
-        <div className="top-0 left-0 w-full h-full absolute -z-10 flex flex-col">
-            <svg viewBox="0 0 1502 200" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-full">
-                <path d="M1451 2341H51C23.3858 2341 1 2318.37 1 2290.75V107V51C1 23.3857 23.3858 1 51 1H650.474C663.726 1 676.436 6.26099 685.812 15.627L723.596 53.373C732.971 62.739 745.681 68 758.933 68H1451C1478.61 68 1501 90.3857 1501 118V182V2291C1501 2318.61 1478.61 2341 1451 2341Z" stroke="#D2FF00" stroke-width="2" />
-            </svg>
+export const Section2 = ({ games }: { games: IGame[] }) => {
+    const [eventTypesSelected, setEventTypesSelected] = useState<ZkNoidEventType[]>([]);
 
-            <div className="border-x-2 border-left-accent flex-grow" />
+    return (
+        <div className='relative flex flex-col'>
+            <div className="top-0 left-0 w-full h-full absolute -z-10 flex flex-col">
+                <svg viewBox="0 0 1502 200" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-full">
+                    <path d="M1451 2341H51C23.3858 2341 1 2318.37 1 2290.75V107V51C1 23.3857 23.3858 1 51 1H650.474C663.726 1 676.436 6.26099 685.812 15.627L723.596 53.373C732.971 62.739 745.681 68 758.933 68H1451C1478.61 68 1501 90.3857 1501 118V182V2291C1501 2318.61 1478.61 2341 1451 2341Z" stroke="#D2FF00" stroke-width="2" />
+                </svg>
 
-            <svg viewBox="0 2142 1502 200" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-full">
-                <path d="M1451 2341H51C23.3858 2341 1 2318.37 1 2290.75V107V51C1 23.3857 23.3858 1 51 1H650.474C663.726 1 676.436 6.26099 685.812 15.627L723.596 53.373C732.971 62.739 745.681 68 758.933 68H1451C1478.61 68 1501 90.3857 1501 118V182V2291C1501 2318.61 1478.61 2341 1451 2341Z" stroke="#D2FF00" stroke-width="2" />
-            </svg>
-        </div>
+                <div className="border-x-2 border-left-accent flex-grow" />
 
-        <div className="top-0 w-full h-full p-10 gap-5 flex flex-col">
-            <div className="flex flex-col gap-3">
-                <div className="text-[32px]">
-                    Events & competitions
-                </div>
-                <div className="flex flex-row gap-3">
-                    <FilterCard text="Past events" />
-                    <FilterCard text="Current events" checked={true} />
-                    <FilterCard text="Upcoming events" />
-                </div>
-                <div className="grid grid-cols-2 gap-5">
-                    <EventCard
-                        headText="Randzu event"
-                        description="Randzu is now live! Invite friend and play a competition"
-                        startsIn="00:00:00"
-                    />
-                    <EventCard
-                        headText="Arkanoid event"
-                        description="Global competition started! Try out your skills in the competition"
-                        startsIn="00:00:00"
-                    />
-                </div>
+                <svg viewBox="0 2142 1502 200" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-full">
+                    <path d="M1451 2341H51C23.3858 2341 1 2318.37 1 2290.75V107V51C1 23.3857 23.3858 1 51 1H650.474C663.726 1 676.436 6.26099 685.812 15.627L723.596 53.373C732.971 62.739 745.681 68 758.933 68H1451C1478.61 68 1501 90.3857 1501 118V182V2291C1501 2318.61 1478.61 2341 1451 2341Z" stroke="#D2FF00" stroke-width="2" />
+                </svg>
             </div>
 
-            <div>
-                <div className="text-[32px]">
-                    Popular genres
-                </div>
-                <div className="grid grid-cols-4 gap-5">
-                    <GenreCard image="image/genres/arcade.svg" text="Arcade" />
-                    <GenreCard image="image/genres/board.svg" text="Board games" />
-                    <GenreCard image="image/genres/lucky.svg" text="Lucky" />
-                    <GenreCard image="image/genres/soon.svg" text="Coming soon" />
-                </div>
-            </div>
-            <div className="flex gap-5">
-                <div className="min-w-[350px]">
-                    <div className="text-[24px] font-bold">Filtration</div>
-                    <div className="flex flex-col gap-3">
-                        <FiltrationBox expanded={true} title="Genre" variants={["Arcade", "Board games", "Lucky"]} />
-                        <FiltrationBox expanded={false} title="Features" variants={[]} />
-
-                        <div className="border-2 rounded-2xl border-left-accent h-[40px] flex items-center justify-center text-[24px] text-left-accent cursor-pointer">
-                            Reset filters
-                        </div>
+            <div className="top-0 w-full h-full p-10 gap-5 flex flex-col">
+                <div className="flex flex-col gap-3">
+                    <div className="text-[32px]">
+                        Events & competitions
                     </div>
-
-                </div>
-                <div className="">
-                    <div className="text-[32px] font-bold">Games</div>
                     <div className="flex flex-row gap-3">
-                        {
-                            GAME_TAGS.map(
-                                x => <div className="p-1 border rounded cursor-pointer">{x}</div>
-                            )
-                        }
+                        {[ZkNoidEventType.PAST_EVENTS, ZkNoidEventType.CURRENT_EVENTS, ZkNoidEventType.UPCOMING_EVENTS]
+                            .map(eventType => (
+                                <FilterCard
+                                    eventType={eventType} 
+                                    typesSelected={eventTypesSelected} 
+                                    setTypesSelected={setEventTypesSelected} 
+                                    selected={eventTypesSelected.includes(eventType)}
+                                />
+                            ))}
                     </div>
-                    <div>
-                        <div className="grid grid-cols-3 gap-5">
-                            {games.map((game) =>
-                                <div
-                                    className="rounded-xl bg-[#252525] min-h-[500px]"
-                                    key={game.id}
-                                >
-                                    <Link
-                                        className="m-5 flex h-full flex-col gap-5"
-                                        href={`/games/${game.id}/${game.defaultPage}`}
-                                    >
-                                        <Image
-                                            src={game.logo}
-                                            alt="Game logo"
-                                            width={220}
-                                            height={251}
-                                            className="w-full"
-                                        />
-                                        <div className="text-[24px]">{game.name}</div>
-                                        <div className="text-[16px] font-plexmono font-[16]">{game.description}</div>
-                                    </Link>
-                                </div>
-                            )}
-                        </div>
+                    <div className="grid grid-cols-2 gap-5  min-h-[352px]">
+                        {GAME_EVENTS.filter(x => eventTypesSelected.includes(getEventType(x)) || eventTypesSelected.length == 0).map(event => (
+                            <EventCard
+                                key={event.name}
+                                headText={event.name}
+                                description={event.description}
+                                event={event}
+                            />
+                        ))}
                     </div>
                 </div>
 
+                <div>
+                    <div className="text-[32px]">
+                        Popular genres
+                    </div>
+                    <div className="grid grid-cols-4 gap-5">
+                        <GenreCard image="image/genres/arcade.svg" text="Arcade" />
+                        <GenreCard image="image/genres/board.svg" text="Board games" />
+                        <GenreCard image="image/genres/lucky.svg" text="Lucky" />
+                        <GenreCard image="image/genres/soon.svg" text="Coming soon" />
+                    </div>
+                </div>
+                <div className="flex gap-5">
+                    <div className="min-w-[350px]">
+                        <div className="text-[24px] font-bold">Filtration</div>
+                        <div className="flex flex-col gap-3">
+                            <FiltrationBox expanded={true} title="Genre" variants={["Arcade", "Board games", "Lucky"]} />
+                            <FiltrationBox expanded={false} title="Features" variants={[]} />
+
+                            <div className="border-2 rounded-2xl border-left-accent h-[40px] flex items-center justify-center text-[24px] text-left-accent cursor-pointer">
+                                Reset filters
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="">
+                        <div className="text-[32px] font-bold">Games</div>
+                        <div className="flex flex-row gap-3">
+                            {
+                                GAME_TAGS.map(
+                                    x => <div className="p-1 border rounded cursor-pointer">{x}</div>
+                                )
+                            }
+                        </div>
+                        <div>
+                            <div className="grid grid-cols-3 gap-5">
+                                {games.map((game) =>
+                                    <div
+                                        className="rounded-xl bg-[#252525] min-h-[500px]"
+                                        key={game.id}
+                                    >
+                                        <Link
+                                            className="m-5 flex h-full flex-col gap-5"
+                                            href={`/games/${game.id}/${game.defaultPage}`}
+                                        >
+                                            <Image
+                                                src={game.logo}
+                                                alt="Game logo"
+                                                width={220}
+                                                height={251}
+                                                className="w-full"
+                                            />
+                                            <div className="text-[24px]">{game.name}</div>
+                                            <div className="text-[16px] font-plexmono font-[16]">{game.description}</div>
+                                        </Link>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
-    </div>
-)
+    )
+}
