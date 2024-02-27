dd9f27bbc4afd07694b286fd526f448d9db906e9
asimaranov <a.simaranov@gmail.com>
2024-02-28 05:59:09 +0700

    Set up eslint + prettier, fixes

---
+++ packages/chain/.eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: ["custom/library"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["warn"],
+    "unicorn/filename-case": ["warn"],
+    "@typescript-eslint/no-unsafe-assignment": ["warn"],
+    "no-console": ["warn"],
+    "prefer-const": ["warn"],
+    "@typescript-eslint/no-unsafe-argument": ["warn"],
+    "@typescript-eslint/no-unsafe-call": ["warn"],
+    "no-await-in-loop": ["warn"],
+    "@typescript-eslint/prefer-for-of": ["warn"],
+    "@typescript-eslint/no-unsafe-call": ["warn"],
+    "@typescript-eslint/no-unsafe-member-access": ["warn"],
+    "@typescript-eslint/no-explicit-any": ["warn"],
+    "@typescript-eslint/no-shadow": ["warn"],
+    "@typescript-eslint/no-unsafe-return": ["warn"],
+    "eqeqeq": ["warn"],
+    "import/no-default-export": ["warn"],
+    "import/first": ["warn"],
+    "@typescript-eslint/no-empty-interface": ["warn"],
+    "no-useless-concat": ["warn"],
+    "@typescript-eslint/naming-convention": ["warn"],
+    "@typescript-eslint/no-empty-function": ["warn"],
+    "@typescript-eslint/no-useless-constructor": ["warn"],
+    "no-param-reassign": ["warn"],
+    "@typescript-eslint/restrict-template-expressions": ["warn"],
+    "@typescript-eslint/no-misused-promises": ["warn"],
+    "@typescript-eslint/no-non-null-assertion": ["warn"]
+  }
+};
