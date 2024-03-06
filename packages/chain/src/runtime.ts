eea9cdde7676c05e87131a27bca9e175bc6988fe
asimaranov <a.simaranov@gmail.com>
2024-03-06 14:49:20 +0700

    Added thimblerig runtime config

--- packages/chain/src/runtime.ts
+++ packages/chain/src/runtime.ts
@@ -1,16 +1,19 @@
-import { ArkanoidGameHub } from './arkanoid/ArkanoidGameHub';
+import { ArkanoidGameHub } from "./arkanoid/ArkanoidGameHub";
 import { Balances } from "./framework/balances";
-import { RandzuLogic } from './randzu/RandzuLogic';
+import { RandzuLogic } from "./randzu/RandzuLogic";
+import { ThimblerigLogic } from "./thimblerig/ThimblerigLogic";
 
 export default {
-    modules: {
-        ArkanoidGameHub,
-        Balances,
-        RandzuLogic
-    },
-    config: {
-        ArkanoidGameHub: {},
-        Balances: {},
-        RandzuLogic: {}
-    },
+  modules: {
+    ArkanoidGameHub,
+    Balances,
+    RandzuLogic,
+    ThimblerigLogic,
+  },
+  config: {
+    ArkanoidGameHub: {},
+    Balances: {},
+    RandzuLogic: {},
+    ThimblerigLogic: {},
+  },
 };
