56dd9b375205851f370893f717b31227764fbb37
asimaranov <a.simaranov@gmail.com>
2024-03-06 14:46:01 +0700

    Added thimblerig game

--- packages/chain/src/index.ts
+++ packages/chain/src/index.ts
@@ -3,6 +3,8 @@ import { ClientAppChain } from '@proto-kit/sdk';
 export * from './client.config.js';
 export * from './randzu/index.js';
 export * from './arkanoid/index.js';
+export * from './thimblerig/index.js';
+
 export * from './engine/index.js';
 export * from './framework/index.js';
 
---
+++ packages/chain/src/thimblerig/ThimblerigLogic.ts
@@ -0,0 +1,183 @@
+import { state, runtimeMethod, runtimeModule } from "@proto-kit/module";
+import type { Option } from "@proto-kit/protocol";
+import { State, StateMap, assert } from "@proto-kit/protocol";
+import {
+  PublicKey,
+  Struct,
+  UInt64,
+  Provable,
+  Bool,
+  Field,
+  Poseidon,
+} from "o1js";
+import { MatchMaker } from "../engine/MatchMaker";
+import type { QueueListItem } from "../engine/MatchMaker";
+
+const BLOCK_PRODUCTION_SECONDS = 5;
+const MOVE_TIMEOUT_IN_BLOCKS = 60 / BLOCK_PRODUCTION_SECONDS;
+
+export class ThimblerigField extends Struct({
+  commitedHash: Field,
+  choice: UInt64,
+}) {}
+
+export class GameInfo extends Struct({
+  player1: PublicKey,
+  player2: PublicKey,
+  currentMoveUser: PublicKey,
+  lastMoveBlockHeight: UInt64,
+  field: ThimblerigField,
+  winner: PublicKey,
+}) {}
+
+@runtimeModule()
+export class ThimblerigLogic extends MatchMaker {
+  // Game ids start from 1
+  @state() public games = StateMap.from<UInt64, GameInfo>(UInt64, GameInfo);
+
+  @state() public gamesNum = State.from<UInt64>(UInt64);
+
+  public override initGame(
+    opponentReady: Bool,
+    opponent: Option<QueueListItem>
+  ): UInt64 {
+    const currentGameId = this.gamesNum
+      .get()
+      .orElse(UInt64.from(0))
+      .add(UInt64.from(1));
+    // Setting active game if opponent found
+    this.games.set(
+      Provable.if(opponentReady, currentGameId, UInt64.from(0)),
+      new GameInfo({
+        player1: this.transaction.sender,
+        player2: opponent.value.userAddress,
+        currentMoveUser: this.transaction.sender,
+        lastMoveBlockHeight: this.network.block.height,
+        field: new ThimblerigField({
+          choice: UInt64.from(0),
+          commitedHash: Field.from(0),
+        }),
+        winner: PublicKey.empty(),
+      })
+    );
+
+    this.gamesNum.set(currentGameId);
+
+    return currentGameId;
+  }
+
+  @runtimeMethod()
+  public commitValue(gameId: UInt64, commitmentHash: Field): void {
+    const sessionSender = this.sessions.get(this.transaction.sender);
+    const sender = Provable.if(
+      sessionSender.isSome,
+      sessionSender.value,
+      this.transaction.sender
+    );
+
+    const game = this.games.get(gameId);
+    assert(game.isSome, "Invalid game id");
+    assert(commitmentHash.greaterThan(0), "Invalid commitment");
+    assert(game.value.field.choice.equals(UInt64.from(0)), "Already chosen");
+    assert(game.value.field.commitedHash.equals(0), "Already commited");
+    assert(
+      game.value.player1.equals(sender),
+      `Only player1 should commit value`
+    );
+    assert(game.value.winner.equals(PublicKey.empty()), `Game finished`);
+
+    game.value.field.commitedHash = commitmentHash;
+    game.value.currentMoveUser = game.value.player2;
+    game.value.lastMoveBlockHeight = this.network.block.height;
+    this.games.set(gameId, game.value);
+  }
+
+  @runtimeMethod()
+  public chooseThumble(gameId: UInt64, choice: UInt64): void {
+    const sessionSender = this.sessions.get(this.transaction.sender);
+    const sender = Provable.if(
+      sessionSender.isSome,
+      sessionSender.value,
+      this.transaction.sender
+    );
+
+    const game = this.games.get(gameId);
+    assert(game.isSome, "Invalid game id");
+    assert(
+      Bool.and(
+        choice.greaterThanOrEqual(UInt64.from(1)),
+        choice.lessThanOrEqual(UInt64.from(3))
+      ),
+      "Invalid choice"
+    );
+    assert(game.value.field.choice.equals(UInt64.from(0)), "Already chosen");
+    assert(game.value.field.commitedHash.greaterThan(0), "Not commited");
+    assert(
+      game.value.player2.equals(sender),
+      `Only player2 should make choice`
+    );
+    assert(game.value.winner.equals(PublicKey.empty()), `Game finished`);
+
+    game.value.field.choice = choice;
+    game.value.currentMoveUser = game.value.player1;
+    game.value.lastMoveBlockHeight = this.network.block.height;
+    this.games.set(gameId, game.value);
+  }
+
+  @runtimeMethod()
+  public revealCommitment(gameId: UInt64, commitmentValue: Field): void {
+    const sessionSender = this.sessions.get(this.transaction.sender);
+    const sender = Provable.if(
+      sessionSender.isSome,
+      sessionSender.value,
+      this.transaction.sender
+    );
+
+    const game = this.games.get(gameId);
+    assert(game.isSome, "Invalid game id");
+    assert(commitmentValue.greaterThan(0), "Invalid commitment");
+    assert(game.value.field.choice.equals(UInt64.from(0)), "Already chosen");
+    assert(game.value.field.commitedHash.equals(0), "Already commited");
+    assert(
+      game.value.player1.equals(sender),
+      `Only player1 should reveal value`
+    );
+    assert(game.value.winner.equals(PublicKey.empty()), `Game finished`);
+
+    Poseidon.hash([commitmentValue]).assertEquals(
+      game.value.field.commitedHash,
+      "Incorrect reveal"
+    );
+    game.value.field.commitedHash = commitmentValue;
+    game.value.currentMoveUser = game.value.player2;
+    game.value.lastMoveBlockHeight = this.network.block.height;
+    game.value.winner = Provable.if(
+      UInt64.from(commitmentValue)
+        .mod(UInt64.from(3))
+        .equals(game.value.field.choice),
+      game.value.player1,
+      game.value.player2
+    );
+
+    this.games.set(gameId, game.value);
+  }
+
+  @runtimeMethod()
+  public proveCommitNotRevealed(gameId: UInt64): void {
+    const sessionSender = this.sessions.get(this.transaction.sender);
+    const sender = Provable.if(
+      sessionSender.isSome,
+      sessionSender.value,
+      this.transaction.sender
+    );
+
+    const game = this.games.get(gameId);
+    assert(game.isSome, "Invalid game id");
+    assert(game.value.field.choice.equals(UInt64.from(0)), "Already chosen");
+    assert(game.value.field.commitedHash.equals(0), "Already commited");
+    assert(
+      game.value.player1.equals(sender),
+      `Only player1 should prove value is not revealed`
+    );
+  }
+}
