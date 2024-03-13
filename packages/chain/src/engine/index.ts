16073a9c7b0743232f3b07a9e50f63ea3db69ed7
asimaranov <a.simaranov@gmail.com>
2024-03-13 00:14:53 +0000

    Added support for opponent timeout proving

--- packages/chain/src/engine/index.ts
+++ packages/chain/src/engine/index.ts
@@ -1,4 +1,4 @@
-import { RoundIdxUser, MatchMaker, PENDING_BLOCKS_NUM_CONST } from './MatchMaker.js';
+import { RoundIdxUser, MatchMaker, PENDING_BLOCKS_NUM_CONST, MOVE_TIMEOUT_IN_BLOCKS } from './MatchMaker.js';
 import { RandomGenerator } from './Random.js';
 
-export { RoundIdxUser, RandomGenerator, MatchMaker, PENDING_BLOCKS_NUM_CONST };
+export { RoundIdxUser, RandomGenerator, MatchMaker, PENDING_BLOCKS_NUM_CONST, MOVE_TIMEOUT_IN_BLOCKS };
--- packages/chain/src/randzu/RandzuLogic.ts
+++ packages/chain/src/randzu/RandzuLogic.ts
@@ -12,10 +12,6 @@ import type { QueueListItem } from "../engine/MatchMaker";
 const RANDZU_FIELD_SIZE = 15;
 const CELLS_LINE_TO_WIN = 5;
 
-const BLOCK_PRODUCTION_SECONDS = 5;
-const MOVE_TIMEOUT_IN_BLOCKS = 60 / BLOCK_PRODUCTION_SECONDS;
-
-
 export class WinWitness extends Struct({
     x: UInt32,
     y: UInt32,
@@ -114,35 +110,6 @@ export class RandzuLogic extends MatchMaker {
         this.gamesNum.set(currentGameId);
 
         return currentGameId;
-    }
-
-    @runtimeMethod()
-    public proveOpponentTimeout(gameId: UInt64): void {
-      const sessionSender = this.sessions.get(this.transaction.sender.value);
-      const sender = Provable.if(sessionSender.isSome, sessionSender.value, this.transaction.sender.value);
-  
-      const game = this.games.get(gameId);
-      assert(game.isSome, "Invalid game id");
-      assert(
-        game.value.currentMoveUser.equals(sender),
-        `Not your move: ${sender.toBase58()}`
-      );
-      assert(
-        game.value.winner.equals(PublicKey.empty()),
-        `Game finished`
-      );
-
-      const isTimeout = this.network.block.height.sub(game.value.lastMoveBlockHeight).greaterThan(UInt64.from(MOVE_TIMEOUT_IN_BLOCKS));
-
-      assert(isTimeout, "Timeout not reached");
-        
-      game.value.currentMoveUser = Provable.if(
-        game.value.currentMoveUser.equals(game.value.player1),
-        game.value.player2,
-        game.value.player1
-      );
-      game.value.lastMoveBlockHeight = this.network.block.height;
-      this.games.set(gameId, game.value);
     }
 
     @runtimeMethod()
