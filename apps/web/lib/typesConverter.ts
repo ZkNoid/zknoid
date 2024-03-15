637a807221a33f17dab71b8eaface9902e3bc116
neogar <work.vatlecov@mail.ru>
2024-03-15 19:27:16 +0500

    feat: fixed typos

--- apps/web/lib/typesConverter.ts
+++ apps/web/lib/typesConverter.ts
@@ -1,8 +1,8 @@
-import {Competition} from 'zknoid-chain-dev';
+import { Competition } from 'zknoid-chain-dev';
 
-import {ICompetition} from './types';
-import {Currency} from '@/constants/currency';
-import {ZkNoidGameGenre} from "@/lib/platform/game_tags";
+import { ICompetition } from './types';
+import { Currency } from '@/constants/currency';
+import { ZkNoidGameGenre } from '@/lib/platform/game_tags';
 
 // Converts contract competition to ICompetition
 export function fromContractCompetition(
@@ -12,16 +12,16 @@ export function fromContractCompetition(
   return {
     id: competitionId,
     seed: competition.seed.toString(),
-    game: {id: 'arkanoid', genre: ZkNoidGameGenre.Arcade}, // only for arkanoid
+    game: { id: 'arkanoid', genre: ZkNoidGameGenre.Arcade }, // only for arkanoid
     title: competition.name.toString(),
     preReg: competition.prereg.toBoolean(),
     preRegDate: {
       start: new Date(+competition.preregStartTime.toString()),
-      end: new Date (+competition.preregEndTime.toString())
+      end: new Date(+competition.preregEndTime.toString()),
     },
     competitionDate: {
-      start: new Date (+competition.competitionStartTime.toString()),
-      end: new Date (+competition.competitionEndTime.toString())
+      start: new Date(+competition.competitionStartTime.toString()),
+      end: new Date(+competition.competitionEndTime.toString()),
     },
     participationFee: competition.participationFee,
     currency: Currency.MINA,
