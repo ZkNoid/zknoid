39215f206d544beae6f385ad84eeca4aff1b634c
neogar <work.vatlecov@mail.ru>
2024-03-21 15:48:20 +0500

    feat: connected Competition.tsx

--- apps/web/components/framework/GameWidget/Competition.tsx
+++ apps/web/components/framework/GameWidget/Competition.tsx
@@ -1,24 +1,58 @@
-export const Competition = ({ startGame }: { startGame: () => void }) => {
+import { ICompetition } from '@/lib/types';
+
+export const Competition = ({
+  startGame,
+  competition,
+}: {
+  startGame: () => void;
+  competition: ICompetition;
+}) => {
   return (
     <div className={'flex h-full w-full flex-col gap-10'}>
       <span className={'w-full text-headline-2 font-bold'}>Competition</span>
       <div className={'flex w-full flex-col gap-2'}>
-        <div className={'flex flex-row gap-8 font-plexsans text-buttons-menu'}>
+        <div
+          className={
+            'grid grid-cols-4 grid-rows-1 gap-8 font-plexsans text-buttons-menu'
+          }
+        >
           <span className={'uppercase text-left-accent'}>Name</span>
-          <span className={'font-normal'}>Arcanoid competition</span>
+          <span className={'font-normal'}>{competition.title}</span>
         </div>
-        <div className={'flex flex-row gap-8 font-plexsans text-buttons-menu'}>
+        <div
+          className={
+            'grid grid-cols-4 grid-rows-1 gap-8 font-plexsans text-buttons-menu'
+          }
+        >
           <span className={'uppercase text-left-accent'}>Funds</span>
-          <span className={'font-normal'}>1000 $MINA</span>
+          <span className={'font-normal'}>
+            {competition.reward} {competition.currency}
+          </span>
         </div>
       </div>
       <div className={'flex flex-col gap-2 font-plexsans text-buttons-menu'}>
         <span className={'uppercase text-left-accent'}>
           Preregistration dates
         </span>
-        <span className={'font-normal'}>15 March 2024 - 17 March 2024</span>
+        <span className={'font-normal'}>
+          {competition.preRegDate.start.toLocaleDateString('en-US', {
+            dateStyle: 'long',
+          })}{' '}
+          -{' '}
+          {competition.preRegDate.end.toLocaleDateString('en-US', {
+            dateStyle: 'long',
+          })}
+        </span>
         <span className={'uppercase text-left-accent'}>Competitions dates</span>
-        <span className={'font-normal'}>15 March 2024 - 17 March 2024</span>
+        <span className={'font-normal'}>
+          {competition.competitionDate.start.toLocaleDateString('en-US', {
+            dateStyle: 'long',
+          })}{' '}
+          -{' '}
+          {competition.competitionDate.end.toLocaleDateString('en-US', {
+            dateStyle: 'long',
+          })}
+        </span>
       </div>
       <div className={'flex flex-col gap-4'}>
         <span className={'w-full text-headline-2 font-bold'}>Rules</span>
