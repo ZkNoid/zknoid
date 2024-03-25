56307ed4ffcb953a41adbb5b2d54eec723b22da7
asimaranov <a.simaranov@gmail.com>
2024-03-25 13:51:58 +0300

    Attempt to fix incorrect max leaf in protokit

--- packages/chain/src/chain.config.ts
+++ packages/chain/src/chain.config.ts
@@ -1,86 +1,8 @@
 import { LocalhostAppChain } from '@proto-kit/cli';
-import { Runtime } from '@proto-kit/module';
-import { VanillaProtocol } from '@proto-kit/protocol';
-import {
-  BlockProducerModule,
-  InMemoryDatabase,
-  LocalTaskQueue,
-  LocalTaskWorkerModule,
-  ManualBlockTrigger,
-  NoopBaseLayer,
-  PrivateMempool,
-  Sequencer,
-  SequencerModule,
-  UnprovenProducerModule,
-  sequencerModule,
-} from '@proto-kit/sequencer';
-import {
-  BlockStorageResolver,
-  GraphqlSequencerModule,
-  GraphqlServer,
-  MempoolResolver,
-  NodeStatusResolver,
-  QueryGraphqlModule,
-  UnprovenBlockResolver,
-  MerkleWitnessResolver,
-} from '@proto-kit/api';
-import {
-  StateServiceQueryModule,
-  BlockStorageNetworkStateModule,
-} from '@proto-kit/sdk';
 import runtime from './runtime';
 
-@sequencerModule()
-class StartupScripts extends SequencerModule {
-  constructor() {
-    super();
-  }
-  async start(): Promise<void> {}
-}
 
 const appChain = LocalhostAppChain.fromRuntime(runtime);
-
-// appChain.configure({
-//   ...appChain.config,
-
-//   Protocol: {
-//     BlockProver: {},
-//     StateTransitionProver: {},
-//     AccountState: {},
-//     BlockHeight: {},
-//     LastStateRoot: {},
-//   },
-
-//   Sequencer: {
-//     Database: {},
-//     UnprovenProducerModule: {},
-
-//     GraphqlServer: {
-//       port: 8080,
-//       host: "0.0.0.0",
-//       graphiql: true,
-//     },
-
-//     Graphql: {
-//       QueryGraphqlModule: {},
-//       MempoolResolver: {},
-//       BlockStorageResolver: {},
-//       NodeStatusResolver: {},
-//       MerkleWitnessResolver: {},
-//       UnprovenBlockResolver: {},
-//     },
-
-//     Mempool: {},
-//     BlockProducerModule: {},
-//     LocalTaskWorkerModule: {},
-//     BaseLayer: {},
-//     TaskQueue: {},
-//     BlockTrigger: {},
-//   },
-
-//   QueryTransportModule: {},
-//   NetworkStateTransportModule: {},
-// });
 
 appChain.configure({
   ...appChain.config,
