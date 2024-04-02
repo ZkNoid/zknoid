4d7687a9c2132e852a2224940b7aed6164f96f89
neogar <work.vatlecov@mail.ru>
2024-04-03 00:57:39 +0500

    feat: add months to DatePicker

--- apps/web/components/ui/games-store/shared/DatePicker.tsx
+++ apps/web/components/ui/games-store/shared/DatePicker.tsx
@@ -3,16 +3,38 @@ import { AnimatePresence, motion } from 'framer-motion';
 import { Button } from '@/components/ui/games-store/shared/Button';
 import { clsx } from 'clsx';
 
-export const DatePicker = ({ trigger }: { trigger: ReactNode }) => {
-  // const currentDate = new Date();
+export const DatePicker = ({
+  trigger,
+  setDateFrom,
+  setDateTo,
+}: {
+  trigger: ReactNode;
+  setDateFrom: (date: string) => void;
+  setDateTo: (date: string) => void;
+}) => {
+  const [currentDate, setCurrentDate] = useState<Date>(new Date());
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [dateFrom, setDateFrom] = useState<number>(0);
-  const [dateTo, setDateTo] = useState<number>(0);
   const [activeDate, setActiveDate] = useState<number | undefined>(undefined);
   const [possibleDate, setPossibleDate] = useState<number | undefined>(
     undefined
   );
   const [pickedDate, setPickedDate] = useState<number | undefined>(undefined);
+  const [currentMonth, setCurrentMonth] = useState<number>(
+    currentDate.getMonth()
+  );
+
+  const getDaysInMonth = (year: number, month: number) => {
+    return new Date(year, month, 0).getDate();
+  };
+
+  const clearDates = () => {
+    setDateTo('');
+    setDateFrom('');
+    setActiveDate(undefined);
+    setPossibleDate(undefined);
+    setPickedDate(undefined);
+  };
+
   return (
     <div className={'relative flex flex-col'}>
       <div className={'cursor-pointer'} onClick={() => setIsOpen(true)}>
@@ -37,21 +59,112 @@ export const DatePicker = ({ trigger }: { trigger: ReactNode }) => {
               onClick={(e) => e.stopPropagation()}
             >
               <div className={'flex w-full flex-row justify-between'}>
-                <div className={'text-left-accent'}> {'<'} </div>
-                <div>Date</div>
-                <div className={'text-left-accent'}> {'>'} </div>
+                <div
+                  className={
+                    'flex w-full max-w-[30%] cursor-pointer  items-center justify-start hover:opacity-80'
+                  }
+                  onClick={() => {
+                    clearDates();
+                    setCurrentMonth(
+                      currentDate.setMonth(currentDate.getMonth() - 1)
+                    );
+                  }}
+                >
+                  <svg
+                    width="9"
+                    height="18"
+                    viewBox="0 0 6 12"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M5 11L1 6L5 1"
+                      stroke="#D2FF00"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </div>
+                <div
+                  className={
+                    'w-full text-center text-[18px]/[18px] font-medium'
+                  }
+                >
+                  {currentDate
+                    .toLocaleDateString('en-US', {
+                      dateStyle: 'long',
+                    })
+                    .split(' ')
+                    .map((item, index) => (index === 1 ? ' ' : item))}
+                </div>
+                <div
+                  className={
+                    'flex w-full max-w-[30%] cursor-pointer items-center justify-end hover:opacity-80'
+                  }
+                  onClick={() => {
+                    clearDates();
+                    setCurrentMonth(
+                      currentDate.setMonth(currentDate.getMonth() + 1)
+                    );
+                  }}
+                >
+                  <svg
+                    width="9"
+                    height="18"
+                    viewBox="0 0 6 12"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M1 11L5 6L1 1"
+                      stroke="#D2FF00"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </div>
               </div>
               <div
                 className={'grid h-full w-full grid-cols-7 grid-rows-5 gap-y-1'}
               >
-                {[...Array(31)].map((_, index) => (
+                <span className="rounded-[5px] p-4 text-center font-plexsans text-main font-medium text-left-accent">
+                  S
+                </span>
+                <span className="rounded-[5px] p-4 text-center font-plexsans text-main font-medium text-left-accent">
+                  M
+                </span>
+                <span className="rounded-[5px] p-4 text-center font-plexsans text-main font-medium text-left-accent">
+                  T
+                </span>
+                <span className="rounded-[5px] p-4 text-center font-plexsans text-main font-medium text-left-accent">
+                  W
+                </span>
+                <span className="rounded-[5px] p-4 text-center font-plexsans text-main font-medium text-left-accent">
+                  T
+                </span>
+                <span className="rounded-[5px] p-4 text-center font-plexsans text-main font-medium text-left-accent">
+                  F
+                </span>
+                <span className="rounded-[5px] p-4 text-center font-plexsans text-main font-medium text-left-accent">
+                  S
+                </span>
+                {[
+                  ...Array(
+                    getDaysInMonth(
+                      currentDate.getFullYear(),
+                      currentDate.getMonth() + 1
+                    )
+                  ),
+                ].map((_, index) => (
                   <span
                     className={clsx(
                       'cursor-pointer rounded-[5px] p-4 text-center font-plexsans text-main font-medium hover:opacity-80',
                       {
                         'border border-left-accent text-left-accent':
                           index + 1 === activeDate || index + 1 === pickedDate,
-                        'opacity:80 border-left-accent text-left-accent':
+                        'opacity:80 border border-left-accent text-left-accent':
                           index + 1 === possibleDate,
                         'rounded-none bg-left-accent text-dark-buttons-text':
                           (index + 1 < activeDate &&
@@ -60,19 +173,24 @@ export const DatePicker = ({ trigger }: { trigger: ReactNode }) => {
                             index + 1 < possibleDate) ||
                           (index + 1 < activeDate && index + 1 > pickedDate) ||
                           (index + 1 > activeDate && index + 1 < pickedDate),
-                        'rounded-r-[5px]':
-                          index + 2 === possibleDate ||
-                          index + 2 === pickedDate,
-                        'rounded-l-[5px]':
-                          index === possibleDate || index === pickedDate,
                         'rounded-r-none':
                           (index + 1 === activeDate &&
                             activeDate < possibleDate) ||
-                          (index + 1 === activeDate && activeDate < pickedDate),
+                          (index + 1 === activeDate &&
+                            activeDate < pickedDate) ||
+                          (index + 1 === pickedDate &&
+                            pickedDate < activeDate) ||
+                          (index + 1 === possibleDate &&
+                            possibleDate < activeDate),
                         'rounded-l-none':
                           (index + 1 === activeDate &&
                             activeDate > possibleDate) ||
-                          (index + 1 === activeDate && activeDate > pickedDate),
+                          (index + 1 === activeDate &&
+                            activeDate > pickedDate) ||
+                          (index + 1 === pickedDate &&
+                            pickedDate > activeDate) ||
+                          (index + 1 === possibleDate &&
+                            possibleDate > activeDate),
                       }
                     )}
                     key={index}
@@ -89,13 +207,41 @@ export const DatePicker = ({ trigger }: { trigger: ReactNode }) => {
                           else {
                             setPickedDate(index + 1);
                             const currentPickedDate = index + 1;
+                            let month = currentDate.getUTCMonth() + 1;
+                            let formattedMonth = month.toString();
+                            if (formattedMonth.length < 2)
+                              formattedMonth = '0' + formattedMonth;
+                            const formattedActiveDate =
+                              activeDate.toString().length < 2
+                                ? '0' + activeDate
+                                : activeDate;
+                            const formattedCurrentPickedDate =
+                              currentPickedDate.toString().length < 2
+                                ? '0' + currentPickedDate
+                                : currentPickedDate;
                             if (activeDate < currentPickedDate) {
-                              setDateFrom(activeDate);
-                              setDateTo(currentPickedDate);
+                              setDateFrom(
+                                `${currentDate
+                                  .getFullYear()
+                                  .toString()}-${formattedMonth}-${formattedActiveDate}`
+                              );
+                              setDateTo(
+                                `${currentDate
+                                  .getFullYear()
+                                  .toString()}-${formattedMonth}-${formattedCurrentPickedDate}`
+                              );
                             }
                             if (activeDate > currentPickedDate) {
-                              setDateFrom(currentPickedDate);
-                              setDateTo(activeDate);
+                              setDateFrom(
+                                `${currentDate
+                                  .getFullYear()
+                                  .toString()}-${formattedMonth}-${formattedCurrentPickedDate}`
+                              );
+                              setDateTo(
+                                `${currentDate
+                                  .getFullYear()
+                                  .toString()}-${formattedMonth}-${formattedActiveDate}`
+                              );
                             }
                           }
                         }
