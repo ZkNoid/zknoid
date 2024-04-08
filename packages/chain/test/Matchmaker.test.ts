e4f4cd557d3acb0f4e8ae6c5bd0153ebfd7d43c1
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-04-08 08:21:22 +0300

    Simple matchmaking game done + initial test

--- packages/chain/test/Matchmaker.test.ts
+++ packages/chain/test/Matchmaker.test.ts
@@ -7,44 +7,113 @@ import {
   Field,
   Bool,
   InferProvable,
+  PublicKey,
 } from 'o1js';
 import { log } from '@proto-kit/common';
 import { Pickles } from 'o1js/dist/node/snarky';
-import { dummyBase64Proof } from 'o1js/dist/node/lib/proof_system';
-import {
-  GameProcessProof,
-  MapGenerationProof,
-  checkMapGeneration,
-  initGameProcess,
-  processTicks,
-} from '../src/ArkanoidGameHub';
-import { GameContext } from '../src/GameContext';
-import { MatchMaker } from '../src/engine/MatchMaker';
-import { RandzuField, RandzuLogic } from '../src/RandzuLogic';
+import { MatchMakerHelper } from './contracts/MatchMakerHelper';
+
+interface IUser {
+  publicKey: PublicKey;
+  privateKey: PrivateKey;
+}
+
+const getTestAccounts = (amount: number): IUser[] => {
+  return [...Array(amount)].map(() => {
+    let privateKey = PrivateKey.random();
+    return {
+      privateKey,
+      publicKey: privateKey.toPublicKey(),
+    };
+  });
+};
 
 log.setLevel('ERROR');
 
 describe('Matchmaker', () => {
-  it.only('Reproduce bug', async () => {
-    const appChain = TestingAppChain.fromRuntime({
-      modules: {
-        RandzuLogic,
-      },
-    });
+  let appChain = TestingAppChain.fromRuntime({
+    MatchMakerHelper,
+  });
 
+  let alice: IUser;
+  let bob: IUser;
+  let user3: IUser;
+  let user4: IUser;
+
+  let game: MatchMakerHelper;
+
+  let register: (user: IUser) => Promise<any>;
+  let claimWin: (user: IUser) => Promise<any>;
+
+  beforeEach(async () => {
     appChain.configurePartial({
       Runtime: {
-        RandzuField: {},
+        MatchMakerHelper: {},
+        Balances: {},
       },
     });
 
-    const alicePrivateKey = PrivateKey.random();
-    const alice = alicePrivateKey.toPublicKey();
-    await appChain.start();
-    appChain.setSigner(alicePrivateKey);
+    [alice, bob, user3, user4] = getTestAccounts(4);
 
-    console.log(
-      await appChain.query.runtime.RandzuLogic.games.get(UInt64.from(0)),
+    await appChain.start();
+
+    game = appChain.runtime.resolve('MatchMakerHelper');
+
+    register = async (user: IUser) => {
+      appChain.setSigner(user.privateKey);
+      let tx = await appChain.transaction(user.publicKey, () => {
+        game.register(user.publicKey, UInt64.zero);
+      });
+
+      await tx.sign();
+      await tx.send();
+
+      let block = await appChain.produceBlock();
+      expect(block!.transactions[0].status.toBoolean()).toBeTruthy();
+    };
+
+    claimWin = async (user: IUser) => {
+      let activeGameId =
+        await appChain.query.runtime.MatchMakerHelper.activeGameId.get(
+          user.publicKey,
+        );
+
+      appChain.setSigner(user.privateKey);
+      let tx = await appChain.transaction(user.publicKey, () => {
+        game.claimWin(activeGameId!);
+      });
+
+      await tx.sign();
+      await tx.send();
+
+      let block = await appChain.produceBlock();
+      expect(block!.transactions[0].status.toBoolean()).toBeTruthy();
+    };
+  });
+
+  it('Simple matchmaking', async () => {
+    // matchmake simple game
+    await register(alice);
+    await register(bob);
+
+    let aliceGameId =
+      await appChain.query.runtime.MatchMakerHelper.activeGameId.get(
+        alice.publicKey,
+      );
+    let bobGameId =
+      await appChain.query.runtime.MatchMakerHelper.activeGameId.get(
+        bob.publicKey,
+      );
+
+    expect(aliceGameId!.toString()).toEqual(bobGameId!.toString());
+
+    // alice win
+    await claimWin(alice);
+
+    // check alice win
+    let gameInfo = await appChain.query.runtime.MatchMakerHelper.games.get(
+      aliceGameId!,
     );
+    expect(gameInfo!.winner.toBase58()).toEqual(alice.publicKey.toBase58());
   });
 });
