537891e962869548c588a10ef6e9576ec4f827fe
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-04-08 09:21:24 +0300

    Added tests for lobby

--- packages/chain/test/Matchmaker.test.ts
+++ packages/chain/test/Matchmaker.test.ts
@@ -42,6 +42,9 @@ describe('Matchmaker', () => {
 
   let game: MatchMakerHelper;
 
+  let createLobby: (user: IUser) => Promise<UInt64>;
+  let joinLobby: (user: IUser, lobbyId: UInt64) => Promise<any>;
+  let startGameInLobby: (user: IUser, lobbyId: UInt64) => Promise<any>;
   let register: (user: IUser) => Promise<any>;
   let claimWin: (user: IUser) => Promise<any>;
   let getGameId: (user: IUser) => Promise<UInt64>;
@@ -59,6 +62,51 @@ describe('Matchmaker', () => {
     await appChain.start();
 
     game = appChain.runtime.resolve('MatchMakerHelper');
+
+    createLobby = async (user: IUser): Promise<UInt64> => {
+      let lobbyId =
+        (await appChain.query.runtime.MatchMakerHelper.lastLobbyId.get()) ||
+        UInt64.from(0);
+      appChain.setSigner(user.privateKey);
+      let tx = await appChain.transaction(user.publicKey, () => {
+        let lobby = game.createLobby();
+      });
+
+      await tx.sign();
+      await tx.send();
+
+      let block = await appChain.produceBlock();
+      expect(block!.transactions[0].status.toBoolean()).toBeTruthy();
+
+      return lobbyId!;
+    };
+
+    joinLobby = async (user: IUser, lobbyId: UInt64) => {
+      appChain.setSigner(user.privateKey);
+      let tx = await appChain.transaction(user.publicKey, () => {
+        game.joinLobby(lobbyId);
+      });
+
+      await tx.sign();
+      await tx.send();
+
+      let block = await appChain.produceBlock();
+      expect(block!.transactions[0].status.toBoolean()).toBeTruthy();
+    };
+
+    startGameInLobby = async (user: IUser, lobbyId: UInt64) => {
+      appChain.setSigner(user.privateKey);
+
+      let tx = await appChain.transaction(user.publicKey, () => {
+        game.startGame(lobbyId);
+      });
+
+      await tx.sign();
+      await tx.send();
+
+      let block = await appChain.produceBlock();
+      expect(block!.transactions[0].status.toBoolean()).toBeTruthy();
+    };
 
     register = async (user: IUser) => {
       appChain.setSigner(user.privateKey);
@@ -96,6 +144,47 @@ describe('Matchmaker', () => {
         user.publicKey,
       ))!;
     };
+  });
+
+  it('Simple lobby', async () => {
+    // Create lobby
+    let lobbyId1 = await createLobby(alice);
+    let lobbyId2 = await createLobby(user3);
+
+    // Join lobby
+    await joinLobby(alice, lobbyId1);
+    await joinLobby(bob, lobbyId1);
+
+    await joinLobby(user3, lobbyId2);
+    await joinLobby(user4, lobbyId2);
+
+    // Start game
+
+    await startGameInLobby(alice, lobbyId1);
+    await startGameInLobby(user3, lobbyId2);
+
+    let aliceGameId = await getGameId(alice);
+    let bobGameId = await getGameId(bob);
+    let user3GameId = await getGameId(user3);
+    let user4GameId = await getGameId(user4);
+
+    expect(aliceGameId.toString()).toEqual(bobGameId.toString());
+    expect(user3GameId.toString()).toEqual(user4GameId.toString());
+    expect(aliceGameId.toString()).not.toEqual(user3GameId.toString());
+
+    // Cliam win
+    await claimWin(bob);
+    await claimWin(user3);
+
+    let gameInfo1 =
+      await appChain.query.runtime.MatchMakerHelper.games.get(aliceGameId);
+
+    expect(gameInfo1!.winner.toBase58()).toEqual(bob.publicKey.toBase58());
+
+    let gameInfo2 =
+      await appChain.query.runtime.MatchMakerHelper.games.get(user4GameId);
+
+    expect(gameInfo2!.winner.toBase58()).toEqual(user3.publicKey.toBase58());
   });
 
   it('Simple matchmaking', async () => {
