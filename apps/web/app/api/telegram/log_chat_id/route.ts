86b97dd4551e2f1b580e5d7d6121300482ac9938
asimaranov <a.simaranov@gmail.com>
2024-04-10 22:23:59 +0300

    Added missed files

---
+++ apps/web/app/api/telegram/log_chat_id/route.ts
@@ -0,0 +1,18 @@
+import TelegramBot from 'node-telegram-bot-api';
+
+
+export async function GET(request: Request) {
+  const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN!, { polling: true });
+
+  bot.on('message', (msg) => {
+    console.log(msg);
+    const chatId = msg.chat.id;
+
+    // send a message to the chat acknowledging receipt of their message
+    bot.sendMessage(chatId, 'Received your message');
+  });
+
+  return Response.json({
+    status: 'ok',
+  });
+}
