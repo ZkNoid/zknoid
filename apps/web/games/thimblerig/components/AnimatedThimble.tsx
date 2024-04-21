0f9b42446b6ca0f24bd90204d6ecc613c1858668
asimaranov <a.simaranov@gmail.com>
2024-04-21 17:58:36 +0300

    Improved thimblerig animations

---
+++ apps/web/games/thimblerig/components/AnimatedThimble.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import Lottie from 'react-lottie';
+
+export default function AnimatedThimble({ animation, onAnimationEnded }: { animation: any, onAnimationEnded: () => void }) {
+  const [animationEnded, setAnimationEnded] = useState(false);
+  return (
+    <Lottie
+      style={{}}
+      options={{
+        loop: false,
+        animationData: animation,
+      }}
+      isPaused={animationEnded}
+      eventListeners={[
+        {
+          eventName: 'enterFrame',
+          callback: ((e: any) => {
+            if (e.currentTime > e.totalTime / 2) {
+              setAnimationEnded(true);
+              onAnimationEnded();
+            }
+          }) as any,
+        },
+      ]}
+    />
+  );
+}
