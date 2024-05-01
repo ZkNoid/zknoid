75a759f99b0bf92bf37aca4fda20a8b7d31dad20
asimaranov <a.simaranov@gmail.com>
2024-05-01 12:24:53 +0300

    Added missed router

---
+++ apps/web/server/api/routers/progress.ts
@@ -0,0 +1,62 @@
+import clientPromise from '@/app/lib/mongodb';
+
+import { z } from 'zod';
+
+import { createTRPCRouter, publicProcedure } from '@/server/api/trpc';
+
+const client = await clientPromise;
+const db = client.db(process.env.MONGODB_DB);
+
+export interface Progress {
+  ARKANOID: boolean[];
+  RANDZU: boolean[];
+  THIMBLERIG: boolean[];
+  UI_TESTS_WEB: boolean[];
+}
+
+// Current game, hostname,
+const EnvContext = z.record(z.string(), z.string());
+
+export const progressRouter = createTRPCRouter({
+  getSolvedQuests: publicProcedure
+    .input(z.object({ userAddress: z.string() }))
+    .query(async ({ ctx, input }) => {
+      return {
+        quests: (
+          await db.collection('statuses').findOne({
+            address: input.userAddress,
+          })
+        )?.statuses as Progress | null,
+      };
+    }),
+  setSolvedQuests: publicProcedure
+    .input(
+      z.object({
+        userAddress: z.string(),
+        section: z.string(),
+        id: z.number(),
+        txHash: z.string(),
+        envContext: EnvContext,
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      await db.collection('statuses').updateOne(
+        { address: input.userAddress },
+        {
+          $set: {
+            [`statuses.${input.section}.${input.id}`]: true,
+            [`ctx.${input.section}.${input.id}`]: input.envContext,
+            [`hashes.${input.section}.${input.id}`]: input.txHash,
+          },
+        },
+        {
+          upsert: true,
+        }
+      );
+      return {
+        userAddress: input.userAddress,
+        section: input.section,
+        id: input.id,
+      };
+    }),
+});
