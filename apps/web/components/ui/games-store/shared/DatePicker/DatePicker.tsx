997267833895912a8e2c9143850fe6a7ffb61ee5
neogar <work.vatlecov@mail.ru>
2024-05-03 14:36:08 +0500

    fix: fixed DatePicker issue

--- apps/web/components/ui/games-store/shared/DatePicker/DatePicker.tsx
+++ apps/web/components/ui/games-store/shared/DatePicker/DatePicker.tsx
@@ -1,7 +1,7 @@
 import { ReactNode, useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { Button } from '@/components/ui/games-store/shared/Button';
-import { clsx } from 'clsx';
+import { DateItem } from '@/components/ui/games-store/shared/DatePicker/DateItem';
 
 export const DatePicker = ({
   trigger,
@@ -15,9 +15,9 @@ export const DatePicker = ({
   const [currentDate, _setCurrentDate] = useState<Date>(new Date());
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  const [activeDate, setActiveDate] = useState<Date>();
-  const [possibleDate, setPossibleDate] = useState<Date>();
-  const [pickedDate, setPickedDate] = useState<Date>();
+  const [activeDate, setActiveDate] = useState<Date | undefined>(undefined);
+  const [possibleDate, setPossibleDate] = useState<Date | undefined>(undefined);
+  const [pickedDate, setPickedDate] = useState<Date | undefined>(undefined);
   const [_currentMonth, setCurrentMonth] = useState<number>(
     currentDate.getMonth()
   );
@@ -36,150 +36,6 @@ export const DatePicker = ({
     setActiveDate(undefined);
     setPossibleDate(undefined);
     setPickedDate(undefined);
-  };
-
-  const DateItem = ({ date }: { date: Date }) => {
-    const [dateTime, _setDateTime] = useState<number>(date.getTime());
-    const [dateDay, _setDateDay] = useState<number>(date.getDay());
-    const [activeDateTime, _setActiveDateTime] = useState<number | undefined>(
-      activeDate?.getTime()
-    );
-    const [pickedDateTime, _setPickedDateTime] = useState<number | undefined>(
-      pickedDate?.getTime()
-    );
-    const [possibleDateTime, _setPossibleDateTime] = useState<
-      number | undefined
-    >(possibleDate?.getTime());
-
-    return (
-      <span
-        className={clsx(
-          'cursor-pointer rounded-[5px] border border-bg-dark p-4 text-center font-plexsans text-main font-medium hover:opacity-80',
-          {
-            'border-left-accent text-left-accent':
-              dateTime === activeDateTime || dateTime === pickedDateTime,
-            'opacity:80 border-left-accent text-left-accent':
-              dateTime === possibleDateTime,
-            'rounded-none border-left-accent bg-left-accent text-dark-buttons-text':
-              (activeDateTime &&
-                dateTime < activeDateTime &&
-                possibleDateTime &&
-                dateTime > possibleDateTime) ||
-              (activeDateTime &&
-                dateTime > activeDateTime &&
-                possibleDateTime &&
-                dateTime < possibleDateTime) ||
-              (activeDateTime &&
-                dateTime < activeDateTime &&
-                pickedDateTime &&
-                dateTime > pickedDateTime) ||
-              (activeDateTime &&
-                dateTime > activeDateTime &&
-                pickedDateTime &&
-                dateTime < pickedDateTime),
-            'rounded-r-none':
-              (activeDateTime &&
-                dateTime === activeDateTime &&
-                possibleDateTime &&
-                activeDateTime &&
-                activeDateTime < possibleDateTime) ||
-              (activeDateTime &&
-                dateTime === activeDateTime &&
-                pickedDateTime &&
-                activeDateTime < pickedDateTime) ||
-              (pickedDateTime &&
-                dateTime === pickedDateTime &&
-                activeDateTime &&
-                pickedDateTime < activeDateTime) ||
-              (possibleDateTime &&
-                dateTime === possibleDateTime &&
-                activeDateTime &&
-                possibleDateTime < activeDateTime),
-            'rounded-l-none':
-              (activeDateTime &&
-                dateTime === activeDateTime &&
-                activeDateTime &&
-                possibleDateTime &&
-                activeDateTime > possibleDateTime) ||
-              (activeDateTime &&
-                dateTime === activeDateTime &&
-                activeDateTime &&
-                pickedDateTime &&
-                activeDateTime > pickedDateTime) ||
-              (pickedDateTime &&
-                dateTime === pickedDateTime &&
-                activeDateTime &&
-                pickedDateTime > activeDateTime) ||
-              (possibleDateTime &&
-                dateTime === possibleDateTime &&
-                activeDateTime &&
-                possibleDateTime > activeDateTime),
-            'col-start-1 col-end-1': dateDay == 0,
-            'col-start-2 col-end-2': dateDay == 1,
-            'col-start-3 col-end-3': dateDay == 2,
-            'col-start-4 col-end-4': dateDay == 3,
-            'col-start-5 col-end-5': dateDay == 4,
-            'col-start-6 col-end-6': dateDay == 5,
-            'col-start-7 col-end-7': dateDay == 6,
-          }
-        )}
-        onMouseOver={() => {
-          if (!pickedDate) if (activeDate) setPossibleDate(date);
-        }}
-        onClick={() => {
-          if (!pickedDate) {
-            if (!activeDate) setActiveDate(date);
-            if (activeDate) {
-              if (date === activeDate) setActiveDate(undefined);
-              else {
-                setPickedDate(date);
-                const formatDate = (item: string | undefined) => {
-                  // @ts-ignore
-                  if (item.length < 2) return '0' + item;
-                  else return item;
-                };
-                const formatMonth = (item: number | undefined) => {
-                  // @ts-ignore
-                  item += 1;
-                  // @ts-ignore
-                  if (item.toString().length < 2) return '0' + item;
-                  else return item;
-                };
-                if (activeDate < date) {
-                  setDateFrom(
-                    `${activeDate?.getFullYear().toString()}-${formatMonth(
-                      activeDate?.getMonth()
-                    )}-${formatDate(activeDate?.getDate().toString())}`
-                  );
-                  setDateTo(
-                    `${date?.getFullYear().toString()}-${formatMonth(
-                      date?.getMonth()
-                    )}-${formatDate(date?.getDate().toString())}`
-                  );
-                }
-                if (activeDate > date) {
-                  setDateFrom(
-                    `${date?.getFullYear().toString()}-${formatMonth(
-                      date?.getMonth()
-                    )}-${formatDate(date?.getDate().toString())}`
-                  );
-                  setDateTo(
-                    `${activeDate?.getFullYear().toString()}-${formatMonth(
-                      activeDate?.getMonth()
-                    )}-${formatDate(activeDate?.getDate().toString())}`
-                  );
-                }
-              }
-            }
-          } else {
-            setPickedDate(undefined);
-            setActiveDate(date);
-          }
-        }}
-      >
-        {date.getDate()}
-      </span>
-    );
   };
 
   return (
@@ -314,6 +170,16 @@ export const DatePicker = ({
                         index + 1
                       )
                     }
+                    activeDateTime={activeDate?.getTime()}
+                    pickedDateTime={pickedDate?.getTime()}
+                    possibleDateTime={possibleDate?.getTime()}
+                    activeDate={activeDate}
+                    pickedDate={pickedDate}
+                    setDateTo={setDateTo}
+                    setDateFrom={setDateFrom}
+                    setActiveDate={setActiveDate}
+                    setPickedDate={setPickedDate}
+                    setPossibleDate={setPossibleDate}
                   />
                 ))}
               </div>
