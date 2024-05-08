6e16862d7bbe28d3439b21fadfe3e391d5b25400
asimaranov <a.simaranov@gmail.com>
2024-05-08 18:40:50 +0300

    Fixed bridge

--- apps/web/components/StoreDepositMenuItem.tsx
+++ apps/web/components/StoreDepositMenuItem.tsx
@@ -1,11 +1,13 @@
-import { useMemo } from 'react';
+import { useContext, useMemo } from 'react';
 import { DepositMenuItem } from './DepositMenuItem';
 import { buildClient } from '@/lib/utils';
 import { DefaultRuntimeModules } from '@/lib/runtimeModules';
 import AppChainClientContext from '@/lib/contexts/AppChainClientContext';
 
 export default function StoreDepositMenuItem() {
-  const defaultClient = useMemo(() => buildClient(DefaultRuntimeModules), []);
+  const contextAppChainClient = useContext(AppChainClientContext);
+
+  const defaultClient = useMemo(() => contextAppChainClient || buildClient(DefaultRuntimeModules), []);
 
   return (
     <AppChainClientContext.Provider value={defaultClient}>
