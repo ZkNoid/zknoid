67acb609da44d33edc154990005aae5f5f5adcc3
asimaranov <a.simaranov@gmail.com>
2024-05-11 00:08:35 +0300

    Added persistent storage

--- packages/chain/src/chain.config.ts
+++ packages/chain/src/chain.config.ts
@@ -1,22 +1,39 @@
 import { LocalhostAppChain } from '@proto-kit/cli';
 import runtime from './runtime';
+import { PrivateKey } from 'o1js';
+import { IntegrationTestDBConfig, createPrismaAppchain } from './utils';
 
-const appChain = LocalhostAppChain.fromRuntime(runtime.modules);
+const PERSISTENT = true;
+let appChain;
 
-appChain.configurePartial({
-  ...appChain.config,
+if (!PERSISTENT) {
+  appChain = LocalhostAppChain.fromRuntime(runtime.modules);
 
-  Runtime: runtime.config,
-  Sequencer: {
-    LocalTaskWorkerModule: {
-      StateTransitionTask: {},
-      RuntimeProvingTask: {},
-      StateTransitionReductionTask: {},
-      BlockReductionTask: {},
-      BlockProvingTask: {},
-      BlockBuildingTask: {},
+  appChain.configurePartial({
+    ...appChain.config,
+
+    Runtime: runtime.config,
+    Sequencer: {
+      LocalTaskWorkerModule: {
+        StateTransitionTask: {},
+        RuntimeProvingTask: {},
+        StateTransitionReductionTask: {},
+        BlockReductionTask: {},
+        BlockProvingTask: {},
+        BlockBuildingTask: {},
+      },
+    }
+  });
+} else {
+  const sender = PrivateKey.random();
+  const { prismaConfig, redisConfig } = IntegrationTestDBConfig;
+  appChain = createPrismaAppchain(prismaConfig, redisConfig);
+  
+  appChain.configurePartial({
+    Signer: {
+      signer: sender,
     },
-  }
-});
+  });
+}
 
 export default appChain as any;
