e6a0b4947db84def7bbd62b5210ead1b3a0f2a4c
asimaranov <a.simaranov@gmail.com>
2024-06-17 23:43:32 +0300

    Updates

--- apps/web/games/lottery/ui/TicketsSection/TicketsSection.tsx
+++ apps/web/games/lottery/ui/TicketsSection/TicketsSection.tsx
@@ -6,15 +6,22 @@ export default function TicketsSection({}: {}) {
   return (
     <div
       className={cn(
-        'relative h-[28.8vw] items-center justify-center rounded-[0.67vw] border border-left-accent',
-        'px-[2vw] py-[2.67vw]'
+        'relative rounded-[0.67vw] border border-left-accent',
+        'px-[2vw] py-[2.67vw] flex flex-col gap-[6vw]'
       )}
     >
       <div className="">
         <div className="mb-[1.33vw] text-[2.13vw]">Buy tickets</div>
         <div className="flex flex-row gap-[1.33vw]">
-          <TicketCard symbols="______"></TicketCard>
-          <BuyInfoCard numberOfTickets={0} cost={0}></BuyInfoCard>
+          <TicketCard symbols="12" amount={1} finalized={false}></TicketCard>
+          <BuyInfoCard numberOfTickets={0} cost={0} buttonActive={false}></BuyInfoCard>
+        </div>
+      </div>
+      <div className="">
+        <div className="mb-[1.33vw] text-[2.13vw]">Previous lotteries</div>
+        <div className="flex flex-row gap-[1.33vw]">
+          <TicketCard symbols="5" amount={0} finalized={false}></TicketCard>
+          <BuyInfoCard numberOfTickets={0} cost={0} buttonActive={false}></BuyInfoCard>
         </div>
       </div>
     </div>
--- apps/web/games/lottery/ui/TicketsSection/ui/BuyInfoCard.tsx
+++ apps/web/games/lottery/ui/TicketsSection/ui/BuyInfoCard.tsx
@@ -1,28 +1,42 @@
+import { ReactNode } from 'react';
+import { VioletLotteryButton } from '../../buttons/VioletLotteryButton';
+import { cn } from '@/lib/helpers';
+
 export default function BuyInfoCard({
   numberOfTickets,
   cost,
+  buttonActive,
 }: {
   numberOfTickets: number;
   cost: number;
+  buttonActive: ReactNode;
 }) {
   return (
     <div className="flex h-[13.53vw] w-[22vw] flex-col rounded-[0.67vw] bg-[#252525] p-[1.33vw] text-[1.07vw]">
       <div className="flex flex-row">
         <div className="text-nowrap">Number of tickets</div>
-        <div className="mb-[0.3vw] w-full  border-spacing-6 border-b	 border-dotted border-[#F9F8F4]"></div>
+        <div className="mx-1 mb-[0.3vw] w-full border-spacing-6 border-b border-dotted border-[#F9F8F4] opacity-50"></div>
         <div className="">{numberOfTickets}</div>
       </div>
       <div className="flex flex-row">
         <div className="text-nowrap">Cost</div>
-        <div className="mb-[0.3vw] w-full  border-spacing-6 border-b	 border-dotted border-[#F9F8F4]"></div>
+        <div className="mx-1 mb-[0.3vw] w-full border-spacing-6 border-b border-dotted border-[#F9F8F4] opacity-50"></div>
         <div className="">{cost}$</div>
       </div>
 
       <div className="mt-auto flex flex-row">
         <div className="text-nowrap">TOTAL AMOUNT</div>
-        <div className="mb-[0.3vw] w-full  border-spacing-6 border-b	 border-dotted border-[#F9F8F4]"></div>
+        <div className="mx-1 mb-[0.3vw] w-full border-spacing-6 border-b border-dotted border-[#F9F8F4] opacity-50"></div>
         <div className="">{cost}$</div>
       </div>
+      <VioletLotteryButton
+        className={cn(
+          'my-[1vw] flex h-[2.13vw] items-center justify-center rounded-[0.33vw] px-[1vw] text-[1.07vw]',
+          !buttonActive && 'opacity-50 cursor-not-allowed'
+        )}
+      >
+        Pay
+      </VioletLotteryButton>
     </div>
   );
 }
