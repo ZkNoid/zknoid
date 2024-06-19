578c0075b1fde37568726f6444a524239639837d
asimaranov <a.simaranov@gmail.com>
2024-06-19 13:35:35 +0300

    Updated lottery contracts

--- apps/web/constants/contracts_cache.ts
+++ apps/web/constants/contracts_cache.ts
@@ -3,204 +3,203 @@ import { head, list } from '@vercel/blob';
 
 const VERCEL_LOTTERY_CACHE = [
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/lagrange-basis-fp-1024-kDfJ1PgEm6MsiqhqB9qLEavsf4hD55',
-    pathname: 'lagrange-basis-fp-1024',
-  },
-  {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/lagrange-basis-fp-1024-pCZCye66UwtrDicIdh8g2bgDu6fgoO.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/lagrange-basis-fp-1024-B1RKpGFymuUsIaJUNxkEh8VYh6MXB4.header',
     pathname: 'lagrange-basis-fp-1024.header',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/lagrange-basis-fp-16384-6UkC2ZIB2nW06auDOapbbrFGK3vRDk.header',
-    pathname: 'lagrange-basis-fp-16384.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/lagrange-basis-fp-1024-a6dXhBvd7x1Z2eyanNKq72ripPwLVq',
+    pathname: 'lagrange-basis-fp-1024',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/lagrange-basis-fp-16384-T5bKfYMJ9s7hbU4X0RsBY26PowawqD',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/lagrange-basis-fp-16384-0kSxyIK0rmGdpXKYnRLPRCQIV0krzm',
     pathname: 'lagrange-basis-fp-16384',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/lagrange-basis-fp-8192-6eKpZ3hQFK7S0zlsFPvnyE6g9Far3H',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/lagrange-basis-fp-16384-v6W2VaRoIKAfMdvG13OrpJ8B30mi5K.header',
+    pathname: 'lagrange-basis-fp-16384.header',
+  },
+  {
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/lagrange-basis-fp-8192-9H6c2aMsZmwh6JsQzgZJw2kWCIasmz',
     pathname: 'lagrange-basis-fp-8192',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/lagrange-basis-fp-8192-D7a94M4eUuD8njV0OWxfna1i16M2Pp.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/lagrange-basis-fp-8192-bSAaEvE1Upu35W7vVwyne78NDEvYJD.header',
     pathname: 'lagrange-basis-fp-8192.header',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/lagrange-basis-fq-16384-2MSDDJ15nkcZo4ErnhACZ6JGeLymQW',
-    pathname: 'lagrange-basis-fq-16384',
-  },
-  {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/lagrange-basis-fq-16384-hmEAnNlHxj56mZE7QLUMp5Nhm4Nolk.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/lagrange-basis-fq-16384-rnYhYR1WtdB8kW6yFvV8pAbfWtAlcP.header',
     pathname: 'lagrange-basis-fq-16384.header',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/srs-fp-65536-COzfjswj7jGC4KRGPNE4QM4QM9y5Ik',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/lagrange-basis-fq-16384-xdPqRSPJ7925Aq26BjHfq2pQoVJq7t',
+    pathname: 'lagrange-basis-fq-16384',
+  },
+  {
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/srs-fp-65536-C5VgRfxJFzp2p4cq6E6OMpX3dulfn9',
     pathname: 'srs-fp-65536',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/srs-fp-65536-xGci9TMHx64BNKkYG2sWg6cDlL36cQ.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/srs-fp-65536-lflkUH1jqlLa8z27NUkyFY692GaRF1.header',
     pathname: 'srs-fp-65536.header',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/srs-fq-32768-9xZyaL0KFkGWUJbfS2V9HhgYFK9oNx.header',
-    pathname: 'srs-fq-32768.header',
-  },
-  {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/srs-fq-32768-impmM6KwlQvJNRsXrc4Y6Tk9XdijkX',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/srs-fq-32768-Abl06ak5M44KurIKxJHEJ1X0tov5yS',
     pathname: 'srs-fq-32768',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-distribution-program-addticket-Kpvt1U3H1mV43HLtXO16LWFwwqQC6U.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/srs-fq-32768-aPrY1nRv1rN1Qa0YGaBqmHl7xDFz9c.header',
+    pathname: 'srs-fq-32768.header',
+  },
+  {
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-distribution-program-addticket-BWpnBCQtHAG6sYFHXtIHYWzKtzpz40.header',
     pathname: 'step-pk-distribution-program-addticket.header',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-distribution-program-addticket-YhUfoNE2H88CWRll1FBnfhQC7w8YGj',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-distribution-program-addticket-TKtMyI4VYtgMN6BSfnVdO6JJLFzDuU',
     pathname: 'step-pk-distribution-program-addticket',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-distribution-program-init-DdRiykr85L7tNEGYSQy9u5KySCRTRD',
-    pathname: 'step-pk-distribution-program-init',
-  },
-  {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-distribution-program-init-xDO122uhUc6r8v9GTIWES8EyOlzqCn.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-distribution-program-init-2dZ4nfosQxGGaKTK75LY0PrOmB29GS.header',
     pathname: 'step-pk-distribution-program-init.header',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-buyticket-F2MfrknQmSMntIRZmlUcAanm4eq5XB',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-distribution-program-init-mhq6EQ9slAyyeW091y24WqW3BK6A0m',
+    pathname: 'step-pk-distribution-program-init',
+  },
+  {
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-buyticket-6JPL4yrglvhNzapgjHRKDbdMJcxkhN.header',
+    pathname: 'step-pk-lottery-buyticket.header',
+  },
+  {
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-buyticket-rzoVPXd5pgrofTjBLfFpp8AKJALkWT',
     pathname: 'step-pk-lottery-buyticket',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-buyticket-mzv7KEugG63m83qcFRvIWKaOIGGnHy.header',
-    pathname: 'step-pk-lottery-buyticket.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-getcommisionforround-LzKIgqeWOCZ9dJeQcN3UhpeXpRuFq1',
+    pathname: 'step-pk-lottery-getcommisionforround',
   },
-  // {
-  //   url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-getcommisionforround-FmrmRDHTg68itIe9leE6sfpNmd6THK',
-  //   pathname: 'step-pk-lottery-getcommisionforround',
-  // },
-  // {
-  //   url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-getcommisionforround-iLVXnKaF8EBCIEwQsPNWgSt6a6rRCj.header',
-  //   pathname: 'step-pk-lottery-getcommisionforround.header',
-  // },
-  // {
-  //   url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-getreward-99VRX2wZOIOirJOL8DnT9plmjkbyD2',
-  //   pathname: 'step-pk-lottery-getreward',
-  // },
-  // {
-  //   url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-getreward-bjqmW5gO85SJ4x1fbHZ0FZ4v598ytM.header',
-  //   pathname: 'step-pk-lottery-getreward.header',
-  // },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-produceresult-ePKptkqZzOS7ANlWHNNRK2abkrvGk0.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-getcommisionforround-aFNfg4k4leaI6QAHH7TD8GH41ZWK9S.header',
+    pathname: 'step-pk-lottery-getcommisionforround.header',
+  },
+  {
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-getreward-ZofiU54AnfJ8InXixKGMzoJXyKH3qB',
+    pathname: 'step-pk-lottery-getreward',
+  },
+  {
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-getreward-jrNf1NqIrEHFIfzdpGKO9NXlwqLIEw.header',
+    pathname: 'step-pk-lottery-getreward.header',
+  },
+  {
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-produceresult-BWoCOXpjxPKf1CmtEZhuzj7Ev6yfuV.header',
     pathname: 'step-pk-lottery-produceresult.header',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-produceresult-lC4ymOAIMk5xHfj9muTnjgjLqbhP2k',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-produceresult-Dq0i16QiAb3NxfwQYTY2JOjhmwJnOv',
     pathname: 'step-pk-lottery-produceresult',
   },
-  // {
-  //   url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-refund-DqzUSFgO55TtyYEVrUGc7dGxLtAnBR',
-  //   pathname: 'step-pk-lottery-refund',
-  // },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-refund-do4YicosnaGsDD2oEp9oYSMdqrsahj.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-refund-3R8oc8pg0rTQ7IqS3QiNlPRen12zWk.header',
     pathname: 'step-pk-lottery-refund.header',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-distribution-program-addticket-aK6pOZGRmaJ3RGn8aCDe5KYlUXrPPG',
-    pathname: 'step-vk-distribution-program-addticket',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-pk-lottery-refund-kFbfAc8KFsbSLekZasWWPfyOUi9oSL',
+    pathname: 'step-pk-lottery-refund',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-distribution-program-addticket-qYcRH4LsIGaU6UBbHnYpHmj7VHNC33.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-distribution-program-addticket-Ffivg25sexZQSXqRoYlhPm5ZXaTVbj.header',
     pathname: 'step-vk-distribution-program-addticket.header',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-distribution-program-init-JtLphlv4pb9nYaUpfdJWeseE6NE0Pt.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-distribution-program-addticket-kbyrdoqavTDgQYqvkaYh9jBQKNDFlK',
+    pathname: 'step-vk-distribution-program-addticket',
+  },
+  {
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-distribution-program-init-2d63fI6Jpw56efwNSmy7LRymV0BXQZ.header',
     pathname: 'step-vk-distribution-program-init.header',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-distribution-program-init-wDWOjdBJ2QAHO9fcVtdMqS02logOGB',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-distribution-program-init-AiL67dehm6Oo5tOhVxWa5oSxRpgRxx',
     pathname: 'step-vk-distribution-program-init',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-buyticket-M9DhIlCiqa3VKUGeEGU2lgp8BnjJsH.header',
-    pathname: 'step-vk-lottery-buyticket.header',
-  },
-  {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-buyticket-hqg2NqCDzAHyz8lfRRWoWOu3k0DkWi',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-buyticket-mpPwcEPTsSWkk4PNT9qjdCSdSe4E6T',
     pathname: 'step-vk-lottery-buyticket',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-getcommisionforround-FxKHJ3cwONrb0n0ly4EIi9llc7FJHq.header',
-    pathname: 'step-vk-lottery-getcommisionforround.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-buyticket-sLd0VU0hH0bkW6uIAJtAurSm5oHvgt.header',
+    pathname: 'step-vk-lottery-buyticket.header',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-getcommisionforround-RszI2KO1gtZYpAN2FDlvMbK8P0gtWX',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-getcommisionforround-VayFNL3uypXANtcih2zOgcD0Xr7jbR',
     pathname: 'step-vk-lottery-getcommisionforround',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-getreward-EQ7eaASyGEdh4Ex6yNchrYgTYnruvd',
-    pathname: 'step-vk-lottery-getreward',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-getcommisionforround-sI531LeY38dpHtjnzUVSjud7z43fTg.header',
+    pathname: 'step-vk-lottery-getcommisionforround.header',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-getreward-picWN5nU3pKjPIWvTUIFFfD2ZmaAfu.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-getreward-9wdhu1qByKPFHPkXo9mVHoEs0yoS2G.header',
     pathname: 'step-vk-lottery-getreward.header',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-produceresult-57jZo3OYFJkqHnFUAGMjdlxCmvZT5I',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-getreward-prExtPJVm1kvxblXunGVirIubcSbCj',
+    pathname: 'step-vk-lottery-getreward',
+  },
+  {
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-produceresult-QkOpKNLQPROzjcx7pORUuTLgsDiWHN',
     pathname: 'step-vk-lottery-produceresult',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-produceresult-jMSJXkKp98JzDLYQnvuBEIlPPd1QLz.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-produceresult-ghM5oZAAWxZZS53x2d9zmR548qPVUJ.header',
     pathname: 'step-vk-lottery-produceresult.header',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-refund-9DZlD340UTvWcra3vQon1wi8p5pUpV',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-refund-diJP5IjwOZB7PN34mpaLPN5QMXR9Ri',
     pathname: 'step-vk-lottery-refund',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-refund-lCg05RXapOaaMYrKNZAAzjQsOlvDGa.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/step-vk-lottery-refund-zqq5kb6Uf6bRf7U3oTa1Bev1OreFrj.header',
     pathname: 'step-vk-lottery-refund.header',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/wrap-pk-distribution-program-LzirsQRxfDGaIRENWgUYEZRA7iPW4B',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/wrap-pk-distribution-program-qyopDPutVzGljFc0E0rs6ECaSotqW7',
     pathname: 'wrap-pk-distribution-program',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/wrap-pk-distribution-program-MTEXIppiV5gq96ddzu5WC5hMxqJLnK.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/wrap-pk-distribution-program-wql59X9LOIlAQdzLO5FskkYLhfwdDp.header',
     pathname: 'wrap-pk-distribution-program.header',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/wrap-pk-lottery-2x0kRKlMh4uffI92RmNy8B21NvA81G.header',
-    pathname: 'wrap-pk-lottery.header',
-  },
-  {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/wrap-pk-lottery-5nhlSnJDLoaEWGEuOmS6w2lJFXsbnj',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/wrap-pk-lottery-6ubPUh9xfRWmZ8CFTCOGIuVMrKfPq5',
     pathname: 'wrap-pk-lottery',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/wrap-vk-distribution-program-FHiQDyjOc5gSwzLuKWCG8vt425cTp1',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/wrap-pk-lottery-EhHVDsb6pTlDlXf0LFaT3frFl2yBn3.header',
+    pathname: 'wrap-pk-lottery.header',
+  },
+  {
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/wrap-vk-distribution-program-BDBm53HVhsU8D3cEFKtLpOdAOYqAj4',
     pathname: 'wrap-vk-distribution-program',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/wrap-vk-distribution-program-vbZdCMlPVhwaaEU3cIhTcukNRqDWIp.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/wrap-vk-distribution-program-VwNfgOKPU0NZMfLg8l4Vjf9fWQ96ho.header',
     pathname: 'wrap-vk-distribution-program.header',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/wrap-vk-lottery-ESIYyLSl7xPCJ36QK1Si9I6rBhTcnA.header',
-    pathname: 'wrap-vk-lottery.header',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/wrap-vk-lottery-nFmAPCy5tyykJ1sdgGtzhvXG12cuZG',
+    pathname: 'wrap-vk-lottery',
   },
   {
-    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/wrap-vk-lottery-oEwNMCl3PsPe88c7KEqDpBTuZ92KQu',
-    pathname: 'wrap-vk-lottery',
+    url: 'https://li4vhe2kc60netyr.public.blob.vercel-storage.com/wrap-vk-lottery-reCHte6SIucFEE6zgdOET6AYugy1xe.header',
+    pathname: 'wrap-vk-lottery.header',
   },
 ];
 
-
 export const LOTTERY_CACHE: ContractCache = {
   cachePath: '/dummy_bridge_cache',
-  files: VERCEL_LOTTERY_CACHE.map(x => x.pathname),
-  urls: VERCEL_LOTTERY_CACHE.map(x => x.url),
+  files: VERCEL_LOTTERY_CACHE.map((x) => x.pathname),
+  urls: VERCEL_LOTTERY_CACHE.map((x) => x.url),
 };
 
 console.log('Lottery cache', LOTTERY_CACHE);
