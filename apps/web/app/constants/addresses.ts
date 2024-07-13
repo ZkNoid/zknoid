419ad27158feb5cf76095fba5166225d35c7e158
asimaranov <a.simaranov@gmail.com>
2024-07-13 11:34:58 +0300

    Latest updates

--- apps/web/app/constants/addresses.ts
+++ apps/web/app/constants/addresses.ts
@@ -4,6 +4,6 @@ export const LOTTERY_ADDRESS: {
   readonly [networkId: string]: string | 'not-deployed';
 } = {
   [NetworkIds.MINA_DEVNET]:
-    'B62qkukYhz7mr9Rq6ucGPjGDpyxvKVQQrkxQSd2QpgHCe5gotY4JTEi',
+    'B62qkoEajPN6Ak23vMQVKcE6AYmLxCJRf2gKvzEmDAh1Qv2L5ibiPSo',
   [NetworkIds.ZEKO_TESTNET]: 'not-deployed',
 };
