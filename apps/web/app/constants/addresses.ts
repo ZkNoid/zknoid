fc4643f2cb90ce35c357cf3058695dc95b8f9287
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-07-19 18:50:19 +0300

    Update contract info

--- apps/web/app/constants/addresses.ts
+++ apps/web/app/constants/addresses.ts
@@ -4,6 +4,6 @@ export const LOTTERY_ADDRESS: {
   readonly [networkId: string]: string | 'not-deployed';
 } = {
   [NetworkIds.MINA_DEVNET]:
-    'B62qkukYhz7mr9Rq6ucGPjGDpyxvKVQQrkxQSd2QpgHCe5gotY4JTEi',
+    'B62qjX889KQovNztwZzNbDh9tamijYL135GEPdt1cCtqggU51v1EPoJ',
   [NetworkIds.ZEKO_TESTNET]: 'not-deployed',
 };
