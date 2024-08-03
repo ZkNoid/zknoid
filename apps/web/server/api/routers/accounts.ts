4890d7a3be6326766159064b59ab03fdb4308e76
neogar <work.vatlecov@mail.ru>
2024-08-04 00:02:20 +0500

    feat: added avatars, change name function

--- apps/web/server/api/routers/accounts.ts
+++ apps/web/server/api/routers/accounts.ts
@@ -21,7 +21,7 @@ export const accountsRouter = createTRPCRouter({
     .input(z.object({ userAddress: z.string(), name: z.string() }))
     .mutation(async ({ input }) => {
       await db.collection('accounts').updateOne(
-        { userAddress: input.userAddress, name: input.name },
+        { userAddress: input.userAddress },
         {
           $set: {
             name: input.name,
@@ -32,6 +32,31 @@ export const accountsRouter = createTRPCRouter({
       return {
         userAddress: input.userAddress,
         name: input.name,
+      };
+    }),
+  checkNameUnique: publicProcedure
+    .input(z.object({ name: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const account = await db
+        .collection('accounts')
+        .findOne({ name: input.name });
+      return !account;
+    }),
+  setAvatar: publicProcedure
+    .input(z.object({ userAddress: z.string(), avatarId: z.number() }))
+    .mutation(async ({ input }) => {
+      await db.collection('accounts').updateOne(
+        { userAddress: input.userAddress },
+        {
+          $set: {
+            avatarId: input.avatarId,
+          },
+        },
+        { upsert: true }
+      );
+      return {
+        userAddress: input.userAddress,
+        avatarId: input.avatarId,
       };
     }),
 });
