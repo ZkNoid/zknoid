6c39e26a1a1a7bfdba1157cc8751ae26ed03f4ba
neogar <work.vatlecov@mail.ru>
2024-08-08 16:08:51 +0500

    fix: fixed NotificationsContainer

--- apps/web/components/shared/Notification/NotificationsContainer.tsx
+++ apps/web/components/shared/Notification/NotificationsContainer.tsx
@@ -7,7 +7,7 @@ import { AnimatePresence } from 'framer-motion';
 export default function NotificationsContainer() {
   const notificationStore = useNotificationStore();
   return (
-    <div className="fixed bottom-[2.5%] right-12 z-30 flex h-screen flex-col-reverse gap-[0.521vw]">
+    <div className="fixed bottom-[2.5%] right-12 z-30 flex h-fit flex-col-reverse gap-[0.521vw]">
       <AnimatePresence>
         {notificationStore.notifications.length > 0 &&
           notificationStore.notifications.map((item) => (
