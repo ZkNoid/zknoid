a2e40e182f508e86adfb8fb63d952f0b401fa32f
asimaranov <a.simaranov@gmail.com>
2024-08-22 22:55:09 +0300

    Number guessing game clean up

--- apps/web/games/number_guessing/config.ts
+++ apps/web/games/number_guessing/config.ts
@@ -1,7 +1,6 @@
 import { createZkNoidGameConfig } from '@/lib/createConfig';
 import { ZkNoidGameType } from '@/lib/platform/game_types';
 import { GuessGame } from 'zknoid-chain-dev';
-import Randzu from './NumberGuessing';
 import { ZkNoidGameFeature, ZkNoidGameGenre } from '@/lib/platform/game_tags';
 
 import { LogoMode } from '@/app/constants/games';
@@ -26,26 +25,4 @@ export const numberGuessingConfig = createZkNoidGameConfig({
     GuessGame,
   },
   page: NumberGuessing,
-});
-
-export const randzuRedirectConfig = createZkNoidGameConfig({
-  id: 'randzu',
-  type: ZkNoidGameType.PVP,
-  name: 'Randzu game',
-  description:
-    'Two players take turns placing pieces on the board attempting to create lines of 5 of their own color',
-  image: '/image/games/randzu.svg',
-  logoMode: LogoMode.CENTER,
-  genre: ZkNoidGameGenre.BoardGames,
-  features: [ZkNoidGameFeature.Multiplayer],
-  isReleased: true,
-  releaseDate: new Date(2024, 0, 1),
-  popularity: 50,
-  author: 'ZkNoid Team',
-  rules:
-    'Randzu is a game played on a 15x15 grid, similar to tic-tac-toe. Two players take turns placing their mark, using balls of different colors. The goal is to get five of your marks in a row, either horizontally, vertically or diagonally.',
-  runtimeModules: {},
-  page: undefined as any,
-  lobby: undefined as any,
-  externalUrl: 'https://proto.zknoid.io/games/randzu/global',
 });
